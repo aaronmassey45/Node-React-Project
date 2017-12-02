c7bf9a1d9a7b503842e4e1c22d61fab302246769
aaronmassey45 <aaronmassey45@gmail.com>
2017-12-02 13:49:40 -0600

    FEATURE/ Connected front end to server and added development script

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
-const express = require('express');
+const app = require('./app');
 
-const app = express();
-let port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}.`);
