c4e32b005d0c59ea44ccbf089f1094e91ad08723
aaronmassey45 <aaronmassey45@gmail.com>
2017-12-06 10:03:37 -0600

    REFACTOR/ Collapse navbar on click. Navbar expands on md devices. Switched a tags to NavLinks. Add alert for unsuccessful logout

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import { NavLink } from 'react-router-dom';
 
 export default class Navbar extends Component {
   handleLogout = async () => {
@@ -7,28 +8,36 @@ export default class Navbar extends Component {
       const token = localStorage.getItem('x-auth');
       await axios.delete('/logout', { headers: { 'x-auth': token } });
       localStorage.setItem('x-auth', null);
-      alert('You logged out successfully');
       window.location.href = '/';
-    } catch (e) {
-      console.log(e);
+    } catch (err) {
+      alert('You aren\'t logged in')
+      console.log(err);
     }
   }
 
   render() {
     return (
-      <nav className='navbar fixed-top navbar-dark bg-dark'>
+      <nav className='navbar fixed-top navbar-expand-md navbar-dark bg-dark'>
         <span className="navbar-brand mb-0 h1">Chowster</span>
         <button className="navbar-toggler" type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <a href="/" className="nav-item nav-link">Home</a>
-            <a href="/login" className="nav-item nav-link">Login</a>
-            <a href="/signup" className="nav-item nav-link">Sign Up</a>
-            <span className="nav-item nav-link fake-link" onClick={this.handleLogout}>Logout</span>
-          </ul>
-        </div>
+        <ul className="collapse navbar-collapse mb-0 justify-content-end p-0" id="navbarNav">
+          <div className="navbar-nav">
+            <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+              <NavLink to="/" exact className="nav-link">Home</NavLink>
+            </li>
+            <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+              <NavLink to="/login" className="nav-link">Login</NavLink>
+            </li>
+            <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+              <NavLink to="/signup" className="nav-link">Sign Up</NavLink>
+            </li>
+            <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+              <span className="nav-link fake-link">Logout</span>
+            </li>
+          </div>
+        </ul>
       </nav>
     );
   }
