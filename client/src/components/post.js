be2f3bdcc31e05bd3becb157db86667c82edabc8
aaronmassey45 <aaronmassey45@gmail.com>
2017-12-08 16:14:25 -0600

    REFACTOR/ Post is now stateful component and added heart as post like button

--- client/src/components/post.js
+++ client/src/components/post.js
@@ -1,38 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import { likePost } from '../store/actions/postActions';
 
-const Post = (props) => {
-  const likePost = async () => {
+class Post extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      liked: false
+    };
+  }
+
+  likePost = async () => {
     try {
-      await props.actions.likePost(props.id);
+      await this.props.actions.likePost(this.props.id);
+      this.setState({ liked: true });
     } catch (err) {
       alert('Couldn\'t like post');
       console.log(err);
     }
   };
 
-  return (
-    <div className="Post">
-      <div className="row">
-        <div className="col-3 my-auto">
-          <img src="https://dummyimage.com/600x400/000/fff&text=Dummy+Header" alt="" className="rounded float-left img-fluid"/>
-        </div>
-        <div className="col my-auto">
-          <div className="text-left col-12">{props.text}</div>
-          <div className="col-12 text-right">
-            <Link to={`/users/account/${props.username}`}>
-              @{props.username}
-            </Link>
-            <span className="fake-link" onClick={likePost}>Like post</span>
+  render () {
+    let heart = !this.state.liked ? '-o' : ' text-danger';
+    const { text, username } = this.props;
+    return (
+      <div className="Post">
+        <div className="row">
+          <div className="col-3 my-auto">
+            <img src="https://dummyimage.com/600x400/000/fff&text=Dummy+Header" alt="" className="rounded float-left img-fluid"/>
+          </div>
+          <div className="col my-auto">
+            <div className="text-left col-12">{text}</div>
+            <div className="col-12 text-right">
+              <Link to={`/users/account/${username}`}>
+                @{username}
+              </Link>
+              <p>
+                <i className={`fa fa-heart${heart} fa-lg fake-link`} onClick={this.likePost}></i>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 const mapDispatchToProps = dispatch => ({
