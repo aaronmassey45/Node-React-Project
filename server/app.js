63bc093241fb185fb0662ca558b12fd41dcafb4f
aaronmassey45 <aaronmassey45@gmail.com>
2017-12-08 17:35:31 -0600

    FEATURE/ Created DELETE /posts/:id route

--- server/app.js
+++ server/app.js
@@ -39,10 +39,10 @@ app.get('/posts', async (req, res) => {
 });
 
 app.get('/posts/user/:id', async (req, res) => {
-  try {
-    let { id } = req.params;
-    if (!ObjectID.isValid(id)) return res.status(404).send();
+  let { id } = req.params;
+  if (!ObjectID.isValid(id)) return res.status(404).send();
 
+  try {
     let posts = await Post.find({ _creator: id });
     res.send(posts);
   } catch (err) {
@@ -51,10 +51,10 @@ app.get('/posts/user/:id', async (req, res) => {
 });
 
 app.get('/post/:id', async (req, res) => {
-  try {
-    let { id } = req.params;
-    if (!ObjectID.isValid(id)) return res.status(404).send();
+  let { id } = req.params;
+  if (!ObjectID.isValid(id)) return res.status(404).send();
 
+  try {
     let post = await Post.findOne({ _id: id });
     res.send(post);
   } catch (err) {
@@ -63,15 +63,31 @@ app.get('/post/:id', async (req, res) => {
 });
 
 app.patch('/post/:id', authenticate, async (req, res) => {
-  try {
-    let { id } = req.params;
-    if (!ObjectID.isValid(id)) return res.status(404).send();
+  let { id } = req.params;
+  if (!ObjectID.isValid(id)) return res.status(404).send();
 
+  try {
     let post = await Post.findOneAndUpdate({ _id: id }, { $inc: { likes: 1 } }, { new: true });
     if (!post) return res.status(404).send();
     res.send({ post });
   } catch (err) {
     res.status(400).send();
+  }
+});
+
+app.delete('/post/:id', authenticate, async (req, res) => {
+  let { id } = req.params;
+  if (!ObjectID.isValid(id)) return res.status(404).send();
+
+  try {
+    const post = await Post.findOneAndRemove({
+      _id: id,
+      _creator: req.user._id
+    });
+    if (!post) return res.status(400).send();
+    res.send({ deleted: post })
+  } catch (err) {
+    res.status(400).send(err);
   }
 });
 
