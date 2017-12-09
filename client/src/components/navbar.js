037f7f5be09669c7bbc98dda68aa057c620acabb
aaronmassey45 <aaronmassey45@gmail.com>
2017-12-09 09:04:34 -0600

    REFACTOR/ Authenticated navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -2,8 +2,15 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import { NavLink, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { isUserAuthenticated } from '../store/actions/loginActions';
 
 class Navbar extends Component {
+  componentWillMount() {
+    this.props.actions.isUserAuthenticated();
+  }
+
   handleLogout = async () => {
     try {
       const token = localStorage.getItem('x-auth');
@@ -28,24 +35,29 @@ class Navbar extends Component {
             <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
               <NavLink to="/" exact className="nav-link">Home</NavLink>
             </li>
-            <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
-              <NavLink to="/login" className="nav-link">Login</NavLink>
-            </li>
-            <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
-              <NavLink to="/signup" className="nav-link">Sign Up</NavLink>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle fake-link" id='navbarDropdown' role='button' data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Account</a>
-              <div className="dropdown-menu bg-dark" aria-labelledby='navbarDropdown'>
-                <div className="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show">
-                  <Link to="/users/me" className="nav-link">Profile</Link>
-                </div>
-                <div className="dropdown-divider"></div>
-                <div className="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show">
-                  <span className="nav-link fake-link" onClick={this.handleLogout}>Logout</span>
-                </div>
-              </div>
-            </li>
+            {
+              this.props.appState.loggedIn ?
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle fake-link" id='navbarDropdown' role='button' data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Account</a>
+                  <div className="dropdown-menu bg-dark" aria-labelledby='navbarDropdown'>
+                    <div className="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+                      <Link to="/users/me" className="nav-link">Profile</Link>
+                    </div>
+                    <div className="dropdown-divider"></div>
+                    <div className="dropdown-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+                      <span className="nav-link fake-link" onClick={this.handleLogout}>Logout</span>
+                    </div>
+                  </div>
+                </li> :
+                <span style={{display: 'inline-flex'}}>
+                  <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+                    <NavLink to="/login" className="nav-link">Login</NavLink>
+                  </li>
+                  <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+                    <NavLink to="/signup" className="nav-link">Sign Up</NavLink>
+                  </li>
+                </span>
+            }
           </div>
         </ul>
       </nav>
@@ -57,4 +69,8 @@ const mapStateToProps = state => ({
   appState: state.appState
  });
 
-export default connect(mapStateToProps)(Navbar);
+ const mapDispatchToProps = dispatch => ({
+   actions: bindActionCreators({isUserAuthenticated}, dispatch)
+ });
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
