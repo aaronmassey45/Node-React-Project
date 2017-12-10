7124dcd147fb85b9fe1483e3bdf3d8896d2c2e9a
aaronmassey45 <aaronmassey45@gmail.com>
2017-12-10 14:38:23 -0600

    STYLE/ Styled & made AccountEdit responsive

--- client/src/components/users-edit.js
+++ client/src/components/users-edit.js
@@ -10,7 +10,13 @@ class AccountEdit extends Component {
     super();
 
     this.state = {
-      redirect: false
+      bio: '',
+      currentPassword: '',
+      email: '',
+      location: '',
+      newPassword: '',
+      redirect: false,
+      username: ''
     };
   }
 
@@ -24,14 +30,83 @@ class AccountEdit extends Component {
     }
   }
 
+  fillInputs = () => {
+    if (this.state.username === '') {
+      const { user } = this.props.appState;
+      this.setState({
+        username: user.username
+      });
+    }
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.id]: e.target.value})
+  }
+
+  handleSubmit = e => {
+    console.log('submitted');
+  }
+
   render() {
-    if (this.state.redirect) return <Redirect to='/' />;
+    let { redirect, username } = this.state;
+    if (redirect) return <Redirect to='/' />;
 
     return (
-      <div className='AccountEdit'>
-        <button className='btn btn-danger text-white' data-toggle='modal' data-target='#deleteModal'>
-          <i className='fa fa-ban' aria-hidden='true'></i> Delete account
-        </button>
+      <div className='AccountEdit text-left'>
+        <div className="card bg-secondary text-white mb-3">
+          <div className="card-body p-2">
+            <h4 className="card-title mb-0">User Account</h4>
+          </div>
+        </div>
+        <div className="card bg-light">
+          <div className="card-header">Basic Information</div>
+          <div className="card-body">
+            <form onSubmit={e => e.preventDefault()}>
+              <div className="form-group row">
+                <label htmlFor="username" className="col-sm-2 col-form-label">Username</label>
+                <div className="col-sm-10">
+                  <input type="text" className="form-control" id="username" onChange={this.handleChange} value={username}/>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label htmlFor="email" className="col-sm-2 col-form-label">Email Address</label>
+                <div className="col-sm-10 my-auto">
+                  <input type="email" className="form-control" id="email" onChange={this.handleChange}/>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label htmlFor="location" className="col-sm-2 col-form-label">Location</label>
+                <div className="col-sm-10">
+                  <input type="text" className="form-control" id="location" onChange={this.handleChange}/>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label htmlFor="bio" className="col-sm-2 col-form-label">Bio</label>
+                <div className="col-sm-10">
+                  <textarea id="bio" className='form-control' rows="3" style={{resize:'none'}} onChange={this.handleChange}></textarea>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label htmlFor="password" className="col-sm-2 col-form-label">New Password</label>
+                <div className="col-sm-10 my-auto">
+                  <input type="password" className="form-control" id="newPassword" minLength='6' onChange={this.handleChange}/>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label htmlFor="password" className="col-sm-2 col-form-label">Current Password</label>
+                <div className="col-sm-10 my-auto">
+                  <input type="password" className="form-control" id="currentPassword" onChange={this.handleChange}/>
+                </div>
+              </div>
+            </form>
+            <button className='btn btn-danger text-white' data-toggle='modal' data-target='#deleteModal'>
+              <i className='fa fa-ban' aria-hidden='true'></i> Delete account
+            </button>
+          </div>
+          <div className="card-footer text-right">
+            <button className="btn btn-success" onClick={this.handleSubmit}>Save</button>
+          </div>
+        </div>
 
         <div className='modal' id='deleteModal' tabIndex='-1' role='dialog'>
           <div className='modal-dialog' role='document'>
@@ -59,8 +134,12 @@ class AccountEdit extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  appState: state.appState
+ });
+
 const mapDispatchToProps = dispatch => ({
   actions: bindActionCreators({deleteUser}, dispatch)
 });
 
-export default connect(null, mapDispatchToProps)(AccountEdit);
+export default connect(mapStateToProps, mapDispatchToProps)(AccountEdit);
