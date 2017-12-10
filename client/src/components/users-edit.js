8c17d7bf22a3dca7d4b3e012c40bfd6a45c060a0
aaronmassey45 <aaronmassey45@gmail.com>
2017-12-09 23:32:50 -0600

    FEATURE/ Added delete account functionality to client side

--- client/src/components/users-edit.js
+++ client/src/components/users-edit.js
@@ -1,22 +1,59 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 class AccountEdit extends Component {
   constructor() {
     super();
 
     this.state = {
-
+      redirect: false
     };
   }
 
-  deleteAccount = () => {
-    console.log('delete');
+  deleteAccount = async () => {
+    try {
+      const token = localStorage.getItem('x-auth');
+      await axios.delete('/users/me', { headers: { 'x-auth': token } });
+      alert('Account deleted.');
+      localStorage.removeItem('x-auth');
+      this.setState({ redirect: true })
+    } catch (err) {
+      alert('Couldn\'t delete account');
+      console.log(err);
+    }
   }
 
   render() {
+    if (this.state.redirect) return <Redirect to='/' />;
+
     return (
       <div className='AccountEdit'>
-        <p><i className='fa fa-ban text-danger' aria-hidden='true' onClick={this.deleteAccount}></i> Delete account</p>
+        <button className='btn btn-danger text-white' data-toggle='modal' data-target='#deleteModal'>
+          <i className='fa fa-ban' aria-hidden='true'></i> Delete account
+        </button>
+
+        <div className='modal' id='deleteModal' tabIndex='-1' role='dialog'>
+          <div className='modal-dialog' role='document'>
+            <div className='modal-content'>
+              <div className='modal-header'>
+                <h5 className='modal-title'>Delete Account</h5>
+                <button className='close' data-dismiss='modal' aria-label='Close'>
+                  <span aria-hidden='true'>&times;</span>
+                </button>
+              </div>
+              <div className='modal-body text-left'>
+                <p>
+                  You are about to delete your account. This is permanent, click 'delete' if you would like to delete your account.
+                </p>
+              </div>
+              <div className='modal-footer'>
+                <button className='btn btn-danger' onClick={this.deleteAccount} data-dismiss='modal'>Delete</button>
+                <button className='btn btn-secondary' data-dismiss='modal'>Cancel</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
