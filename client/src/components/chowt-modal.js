13d0d13bde7c5b626af2e9bd0463af67e89124c7
aaronmassey45 <aaronmassey45@gmail.com>
2017-12-14 20:26:56 -0600

    REFACTOR/ Can only send location if food truck account

--- client/src/components/chowt-modal.js
+++ client/src/components/chowt-modal.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { connect } from 'react-redux';
 
-export default class FloatingChowt extends Component {
-  state = { chowt: '' }
+class FloatingChowt extends Component {
+  state = {
+    chowt: '',
+    sendLocation: false
+   }
 
   handleChange = e => {
+    if (e.target.id === 'sendLocation') return this.setState({ sendLocation: !this.state.sendLocation });
     this.setState({ chowt: e.target.value });
   }
 
   submitChowt = async (e) => {
     e.preventDefault();
+
     try {
       const token = localStorage.getItem('x-auth');
       const headers = { 'x-auth': token };
-      await axios.post('/chowt', { text: this.state.chowt }, { headers });
-      this.setState({ chowt: '' });
+      if (this.state.sendLocation && this.props.appState.user.isAFoodTruck) {
+        if (!navigator.geolocation) {
+          this.setState({ sendLocation: false });
+          return alert('Geoloction not supported by your browser');
+        }
+
+        navigator.geolocation.getCurrentPosition(async (position) => {
+          const URL =  `https://www.google.com/maps?q=${position.coords.latitude},${position.coords.longitude}`;
+          const locationMessage = `<p class='mb-0'><small><a href='${URL}' target="_blank">My Location</a></small></p>`;
+          await axios.post('/chowt', { text: this.state.chowt + locationMessage }, { headers });
+          this.setState({ chowt: '', key: Math.random()*10000, sendLocation: false });
+        })
+      } else {
+        await axios.post('/chowt', { text: this.state.chowt }, { headers });
+        this.setState({ chowt: '', key: Math.random()*10000 });
+      }
     } catch (err) {
-      console.log(err);
+      alert('Post failed')
     }
   }
 
@@ -55,6 +75,16 @@ export default class FloatingChowt extends Component {
                       </button>
                     </span>
                   </div>
+                  {
+                    this.props.appState.user.isAFoodTruck ?
+                    <div className="form-check text-right mb-0 mt-1">
+                      <label className="form-check-label">
+                        <input type="checkbox" className="form-check-input" id='sendLocation' checked={this.state.sendLocation} onChange={this.handleChange} />
+                        Send Location
+                      </label>
+                    </div> :
+                    ''
+                  }
                 </form>
               </div>
             </div>
@@ -64,3 +94,9 @@ export default class FloatingChowt extends Component {
     );
   }
 }
+
+const mapStateToProps = state => ({
+  appState: state.appState
+ });
+
+export default connect(mapStateToProps)(FloatingChowt);
