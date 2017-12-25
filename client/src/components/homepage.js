bd55c5c6a758e43c9767458d1bb06fb07e81827a
aaronmassey45 <aaronmassey45@gmail.com>
2017-12-25 14:41:08 -0600

    STYLE/ Added margin bottom

--- client/src/components/homepage.js
+++ client/src/components/homepage.js
@@ -5,7 +5,7 @@ import PostList from './posts-list';
 export default class HomePage extends Component {
   render() {
     return (
-      <div className="HomePage container">
+      <div className="HomePage container mb-2">
         <h1 className="home-heading">Chowster</h1>
         <div className="card">
           <div className="card-header">Most Recent Posts</div>
