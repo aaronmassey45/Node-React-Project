b541cedd29edc88de5786fbc1300f8156bf0b663
aaronmassey45 <aaronmassey45@gmail.com>
2017-12-28 14:30:03 -0600

    REFACTOR/ Added spinner while attempting to sign up. Input is managed by react state now

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -10,7 +10,7 @@ class SignUp extends Component {
     isAFoodTruck: false,
     email: '',
     password: '',
-    redirect: false,
+    signupFailed: false,
     username: ''
   };
 
@@ -25,23 +25,29 @@ class SignUp extends Component {
     e.preventDefault();
     try {
       const { isAFoodTruck, email, password, username } = this.state;
-      await this.props.actions.signup({
+      await this.props.signup({
         isAFoodTruck,
         email,
         password,
         username
       });
-      alert('Signup successful!');
-      this.setState({ redirect: true });
     } catch (err) {
-      alert('Signup failed!');
+      this.setState({ signupFailed: true });
       console.log(err);
     }
   };
 
   render() {
-    if (this.state.redirect || this.props.appState.loggedIn)
-      return <Redirect to="/users/me" />;
+    const { isFetching, loggedIn } = this.props.appState;
+    if (loggedIn) return <Redirect to="/users/me" />;
+
+    const {
+      isAFoodTruck,
+      email,
+      password,
+      signupFailed,
+      username
+    } = this.state;
 
     return (
       <div className="SignUp container my-1">
@@ -49,72 +55,89 @@ class SignUp extends Component {
           <div className="col-sm-6 mx-auto">
             <div className="card">
               <div className="card-body">
-                <form onSubmit={this.handleSubmit}>
-                  <div className="form-group">
-                    <label htmlFor="email">Email Address</label>
-                    <input
-                      type="email"
-                      className="form-control"
-                      id="email"
-                      aria-describedby="emailHelp"
-                      placeholder="Enter email"
-                      onChange={this.handleChange}
-                    />
-                    <small
-                      id="emailHelp"
-                      className="form-text text-muted text-left">
-                      We'll never share your email with anyone else.
-                    </small>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="username">Username</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="username"
-                      placeholder="Create a username"
-                      aria-describedby="userHelp"
-                      onChange={this.handleChange}
-                    />
-                    <div className="row" id="userHelp">
-                      <div className="col-6 text-left">
-                        <small className="form-text text-muted">Min: 4</small>
-                      </div>
-                      <div className="col-6 text-right">
-                        <small className="form-text text-muted">Max: 20</small>
+                {isFetching ? (
+                  <i className="fa fa-spinner fa-pulse fa-3x fa-fw" />
+                ) : (
+                  <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                      <label htmlFor="email">Email Address</label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="email"
+                        aria-describedby="emailHelp"
+                        placeholder="Enter email"
+                        onChange={this.handleChange}
+                        value={email}
+                      />
+                      <small
+                        id="emailHelp"
+                        className="form-text text-muted text-left">
+                        We'll never share your email with anyone else.
+                      </small>
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="username">Username</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="username"
+                        placeholder="Create a username"
+                        aria-describedby="userHelp"
+                        onChange={this.handleChange}
+                        value={username}
+                      />
+                      <div className="row" id="userHelp">
+                        <div className="col-6 text-left">
+                          <small className="form-text text-muted">Min: 4</small>
+                        </div>
+                        <div className="col-6 text-right">
+                          <small className="form-text text-muted">
+                            Max: 20
+                          </small>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="password"
-                      placeholder="Password"
-                      aria-describedby="passHelp"
-                      onChange={this.handleChange}
-                    />
-                    <small className="form-text text-muted text-left">
-                      Min: 6
-                    </small>
-                  </div>
-                  <div className="form-check">
-                    <label className="form-check-label">
+                    <div className="form-group">
+                      <label htmlFor="password">Password</label>
                       <input
-                        type="checkbox"
-                        className="form-check-input"
-                        id="isAFoodTruck"
+                        type="password"
+                        className="form-control"
+                        id="password"
+                        placeholder="Password"
+                        aria-describedby="passHelp"
                         onChange={this.handleChange}
+                        value={password}
                       />
-                      Is this a food truck account?
-                    </label>
-                  </div>
-                  <button type="submit" className="btn btn-primary btn-block">
-                    Submit
-                  </button>
-                </form>
+                      <small className="form-text text-muted text-left">
+                        Min: 6
+                      </small>
+                    </div>
+                    <div className="form-check">
+                      <label className="form-check-label">
+                        <input
+                          type="checkbox"
+                          className="form-check-input"
+                          id="isAFoodTruck"
+                          onChange={this.handleChange}
+                          checked={isAFoodTruck}
+                        />
+                        Is this a food truck account?
+                      </label>
+                    </div>
+                    <button type="submit" className="btn btn-primary btn-block">
+                      Submit
+                    </button>
+                  </form>
+                )}
               </div>
+              {signupFailed ? (
+                <div className="alert alert-danger" role="alert">
+                  Signup failed! Try again.
+                </div>
+              ) : (
+                ''
+              )}
               <div className="card-footer">
                 Already a user? <Link to="/login">Login</Link>
               </div>
@@ -130,8 +153,8 @@ const mapStateToProps = state => ({
   appState: state.appState
 });
 
-const mapDispatchToProps = dispatch => ({
-  actions: bindActionCreators({ signup }, dispatch)
-});
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ signup }, dispatch);
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
