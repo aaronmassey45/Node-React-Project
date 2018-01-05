0155b503204fb48cbd0170fefa1452d4804da264
aaronmassey45 <aaronmassey45@gmail.com>
2018-01-04 18:25:10 -0600

    REFACTOR/ Connected to handle-modal HOC

--- client/src/components/chowt-modal.js
+++ client/src/components/chowt-modal.js
@@ -1,39 +1,42 @@
 import React, { Component } from 'react';
 
 import Chowt from './chowt';
+import handleModal from './HOCs/handle-modal';
 
-export default class FloatingChowt extends Component {
+class FloatingChowt extends Component {
   render() {
-    return (
+    const { hide, show, showModal } = this.props;
+    return showModal ? (
       <div>
-        <div className="FloatingChowt">
-          <button
-            className="btn btn-gray fake-link"
-            data-toggle="modal"
-            data-target="#chowtModal">
-            <i className="fa fa-paper-plane" />
-          </button>
-        </div>
-
-        <div className="modal fade" id="chowtModal" tabIndex="-1" role="dialog">
+        <div
+          className="modal fade show"
+          tabIndex="-1"
+          role="dialog"
+          style={{ display: 'block' }}>
           <div className="modal-dialog" role="document">
             <div className="modal-content">
               <div className="modal-header">
                 <h5 className="modal-title">Send Chowt</h5>
-                <button
-                  className="close"
-                  data-dismiss="modal"
-                  aria-label="Close">
+                <button className="close" onClick={hide}>
                   <span>&times;</span>
                 </button>
               </div>
               <div className="modal-body">
-                <Chowt />
+                <Chowt hide={hide} />
               </div>
             </div>
           </div>
         </div>
+        <div className="modal-backdrop show fade" />
+      </div>
+    ) : (
+      <div className="FloatingChowt">
+        <button className="btn btn-gray fake-link" onClick={show}>
+          <i className="fa fa-paper-plane" />
+        </button>
       </div>
     );
   }
 }
+
+export default handleModal(FloatingChowt);
