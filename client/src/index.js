586559cd3b01f1ad53b62fc580c2076596e08662
Aaron <aaronmassey45@gmail.com>
2018-05-10 12:53:53 -0500

    Moved html scripts to imports in react

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,10 @@ import { Provider } from 'react-redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from '../src/store/store';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 ReactDOM.render(
   <Provider store={store}>
