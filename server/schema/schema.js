6074cd0b973a203a32637497aafe8732df064658
Aaron <aaronmassey45@gmail.com>
2018-06-03 09:54:18 -0500

    Added mutations to graphql schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,9 @@
 const graphql = require('graphql');
 
 const RootQuery = require('./types/rootQueryType');
+const mutations = require('./mutations');
 
 module.exports = new graphql.GraphQLSchema({
   query: RootQuery,
+  mutation: mutations,
 });
