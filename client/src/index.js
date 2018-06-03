e64a4922a4313a4094bf5eeda4993e62e04dcfb9
Aaron <aaronmassey45@gmail.com>
2018-06-03 12:43:42 -0500

    Connected to graphql apollo client

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+// import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -11,9 +14,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-rater/lib/react-rater.css';
 import './App.css';
 
+const cache = new InMemoryCache({
+  dataIdFromObject: o => o.id,
+});
+
+const client = new ApolloClient({
+  cache,
+  credentials: 'same-origin',
+  uri: '/api',
+});
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </Provider>,
   document.getElementById('root')
 );
