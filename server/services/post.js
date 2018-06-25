3faaefab97df9589f2340aa98de377d447ea3226
Aaron <aaronmassey45@gmail.com>
2018-06-25 09:29:40 -0500

    Created function to like and unlike post

--- server/services/post.js
+++ server/services/post.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Post = mongoose.model('post');
 
+const ObjectId = mongoose.Types.ObjectId;
+
 const chowt = async (text, location, user) => {
   try {
     const post = new Post({
@@ -16,4 +18,35 @@ const chowt = async (text, location, user) => {
   }
 };
 
-module.exports = { chowt };
+const likeChowt = async (id, currentUser) => {
+  try {
+    if (!ObjectId.isValid(id)) throw new Error('Invalid chowt id.');
+
+    const post = await Post.findById(id);
+    if (!post) throw new Error('No post found');
+
+    const haveLiked = post.likedBy.some(user => user.equals(currentUser._id));
+
+    if (haveLiked) {
+      post.likedBy = post.likedBy.filter(
+        user => (user.equals(currentUser._id) ? false : true)
+      );
+      currentUser.likedPosts = currentUser.likedPosts.filter(
+        post => (post.equals(id) ? false : true)
+      );
+    } else {
+      post.likedBy.push(currentUser._id);
+      currentUser.likedPosts.push(id);
+    }
+
+    await post.save();
+    await currentUser.save();
+
+    return post;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
+
+module.exports = { chowt, likeChowt };
