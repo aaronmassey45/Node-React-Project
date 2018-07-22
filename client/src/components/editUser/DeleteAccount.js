e15b72d9324264f38d490bb44cea9502a95a1f4d
Aaron <aaronmassey45@gmail.com>
2018-07-22 14:13:15 -0500

    GraphQL refactor to delete user

--- client/src/components/editUser/DeleteAccount.js
+++ client/src/components/editUser/DeleteAccount.js
@@ -1,38 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { graphql, compose } from 'react-apollo';
+import { withRouter } from 'react-router-dom';
 
-const DeleteAccount = ({ deleteUser }) => {
-  return (
-    <div className="modal" id="deleteModal" tabIndex="-1" role="dialog">
-      <div className="modal-dialog" role="document">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title">Delete Account</h5>
-            <button className="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div className="modal-body text-left">
-            <p>
-              You are about to delete your account. This is permanent, click
-              'Delete' if you would like to delete your account.
-            </p>
-          </div>
-          <div className="modal-footer">
-            <button
-              className="btn btn-danger"
-              onClick={deleteUser}
-              data-dismiss="modal"
-            >
-              Delete
-            </button>
-            <button className="btn btn-secondary" data-dismiss="modal">
-              Cancel
-            </button>
+import mutation from '../../mutations/DeleteUser';
+import query from '../../queries/CurrentUser';
+
+class DeleteAccount extends Component {
+  deleteAccount = async () => {
+    try {
+      await this.props.mutate({
+        refetchQueries: [
+          {
+            query,
+            variables: {
+              withLikedPosts: false,
+            },
+          },
+        ],
+      });
+      localStorage.removeItem('x-auth');
+      this.props.history.push('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  render() {
+    return (
+      <div className="modal" id="deleteModal" tabIndex="-1" role="dialog">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Delete Account</h5>
+              <button className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body text-left">
+              <p>
+                You are about to delete your account. This is permanent, click
+                'Delete' if you would like to delete your account.
+              </p>
+            </div>
+            <div className="modal-footer">
+              <button
+                className="btn btn-danger"
+                onClick={this.deleteAccount}
+                data-dismiss="modal"
+              >
+                Delete
+              </button>
+              <button className="btn btn-secondary" data-dismiss="modal">
+                Cancel
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default DeleteAccount;
+export default compose(graphql(mutation), withRouter)(DeleteAccount);
