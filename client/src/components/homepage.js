8d4e7e86fce83c8263ca3ec69e18f83457e0d148
Aaron <aaronmassey45@gmail.com>
2018-07-23 11:22:02 -0500

    Temporarily remove feed from homepage

--- client/src/components/homepage.js
+++ client/src/components/homepage.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import PostList from './posts-list';
-
 export default class HomePage extends Component {
   render() {
     return (
@@ -9,7 +7,6 @@ export default class HomePage extends Component {
         <h1 className="home-heading">Chowster</h1>
         <div className="card">
           <div className="card-header">Most Recent Posts</div>
-          <PostList type="home" />
         </div>
       </div>
     );
