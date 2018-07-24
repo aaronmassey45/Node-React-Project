e3aa292d16a93df451e15a7411897e802faac07e
Aaron <aaronmassey45@gmail.com>
2018-07-24 12:15:25 -0500

    Updated CurrentUser query to send additional data if needed. Set default values to false so it only needs to be specified when true

--- client/src/queries/CurrentUser.js
+++ client/src/queries/CurrentUser.js
@@ -1,12 +1,22 @@
 import gql from 'graphql-tag';
 
 export default gql`
-  query CurrentUser($withLikedPosts: Boolean!) {
+  query CurrentUser(
+    $withLikedPosts: Boolean = false
+    $withEditingData: Boolean = false
+  ) {
     me {
       id
       username
       likedPosts @include(if: $withLikedPosts) {
         id
+      }
+      ... on User @include(if: $withEditingData) {
+        email
+        location
+        profileImg
+        bio
+        isAFoodTruck
       }
     }
   }
