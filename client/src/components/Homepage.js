b5ec0f5f90cfea0ebed5ecc996f01ecb8a428514
Aaron <aaronmassey45@gmail.com>
2018-09-22 11:32:49 -0500

    graphql integration

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+
+import Post from './post';
+import CurrentUser from '../queries/CurrentUser';
+import GetPosts from '../queries/GetPosts';
 
 export default class HomePage extends Component {
   render() {
     return (
-      <div className="HomePage container mb-2">
-        <h1 className="home-heading">Chowster</h1>
-        <div className="card">
-          <div className="card-header">Most Recent Posts</div>
-        </div>
-      </div>
+      <Query query={GetPosts}>
+        {({ loading: loadingOne, data: { posts } }) => (
+          <Query query={CurrentUser} variables={{ withLikedPosts: true }}>
+            {({ loading: loadingTwo, data: { me } }) => (
+              <div className="HomePage container mb-2">
+                <h1 className="home-heading">Chowster</h1>
+                <div className="card">
+                  <div className="card-header">Most Recent Posts</div>
+                  <div className="card-body">
+                    {loadingOne || loadingTwo ? (
+                      <div className="text-center">
+                        <i className="fa fa-spinner fa-pulse fa-3x fa-fw" />
+                      </div>
+                    ) : (
+                      posts.map(post => (
+                        <Post
+                          post={post}
+                          profile={post._creator}
+                          me={me}
+                          key={post.id}
+                        />
+                      ))
+                    )}
+                  </div>
+                </div>
+              </div>
+            )}
+          </Query>
+        )}
+      </Query>
     );
   }
 }
