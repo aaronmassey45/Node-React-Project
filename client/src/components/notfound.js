cbb989da804b91fea22b76df9df79cd869025adc
Aaron <aaronmassey45@gmail.com>
2018-11-17 12:09:59 -0600

    Changed link to look like a button

--- client/src/components/notfound.js
+++ client/src/components/notfound.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NotFound = () => {
-  return (
-    <div className="NotFound">
-      <h1>404 Not Found</h1>
-      <h3>The page you are looking for does not exist</h3>
-      <h5>
-        <Link to="/">Return home</Link>
-      </h5>
-    </div>
-  );
-};
+const NotFound = () => (
+  <div className="NotFound">
+    <h1>404 Not Found</h1>
+    <h3>The page you are looking for does not exist</h3>
+    <h5>
+      <Link className="btn btn-secondary" to="/">
+        Return home
+      </Link>
+    </h5>
+  </div>
+);
 
 export default NotFound;
