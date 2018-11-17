c487b467958ce41c92fb3ee44250e2b74a5d88fe
Aaron <aaronmassey45@gmail.com>
2018-11-17 11:59:39 -0600

    Changed to functional component

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -5,39 +5,39 @@ import Post from './Post';
 import CurrentUser from '../queries/CurrentUser';
 import GetPosts from '../queries/GetPosts';
 
-export default class HomePage extends Component {
-  render() {
-    return (
-      <Query query={GetPosts}>
-        {({ loading: loadingOne, data: { posts } }) => (
-          <Query query={CurrentUser} variables={{ withLikedPosts: true }}>
-            {({ loading: loadingTwo, data: { me } }) => (
-              <div className="HomePage container mb-2">
-                <h1 className="home-heading">Chowster</h1>
-                <div className="card">
-                  <div className="card-header">Most Recent Posts</div>
-                  <div className="card-body">
-                    {loadingOne || loadingTwo ? (
-                      <div className="text-center">
-                        <i className="fa fa-spinner fa-pulse fa-3x fa-fw" />
-                      </div>
-                    ) : (
-                      posts.map(post => (
-                        <Post
-                          post={post}
-                          profile={post._creator}
-                          me={me}
-                          key={post.id}
-                        />
-                      ))
-                    )}
-                  </div>
+const HomePage = () => {
+  return (
+    <Query query={GetPosts}>
+      {({ loading: loadingOne, data: { posts } }) => (
+        <Query query={CurrentUser} variables={{ withLikedPosts: true }}>
+          {({ loading: loadingTwo, data: { me } }) => (
+            <div className="HomePage container mb-2">
+              <h1 className="home-heading">Chowster</h1>
+              <div className="card">
+                <div className="card-header">Most Recent Posts</div>
+                <div className="card-body">
+                  {loadingOne || loadingTwo ? (
+                    <div className="text-center">
+                      <i className="fa fa-spinner fa-pulse fa-3x fa-fw" />
+                    </div>
+                  ) : (
+                    posts.map(post => (
+                      <Post
+                        post={post}
+                        profile={post._creator}
+                        me={me}
+                        key={post.id}
+                      />
+                    ))
+                  )}
                 </div>
               </div>
-            )}
-          </Query>
-        )}
-      </Query>
-    );
-  }
-}
+            </div>
+          )}
+        </Query>
+      )}
+    </Query>
+  );
+};
+
+export default HomePage;
