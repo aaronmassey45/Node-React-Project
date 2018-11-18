5884eb5d0e6e2e30b59e7a013c977bf43eba562a
Aaron <aaronmassey45@gmail.com>
2018-11-18 13:05:54 -0600

    Now uses Query component for graphQL queries

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Rater from 'react-rater';
-import { graphql, compose } from 'react-apollo';
+import { Query } from 'react-apollo';
 
 import Post from './Post';
 import Chowt from './Chowt';
-import FetchUser from '../queries/FetchUser';
-import CurrentUser from '../queries/CurrentUser';
+import FETCH_USER from '../queries/FetchUser';
+import CURRENT_USER from '../queries/CurrentUser';
 
-class User extends Component {
+export default class User extends Component {
   rateUser = async rate => {
     try {
       if (rate.type === 'click') {
@@ -28,8 +28,8 @@ class User extends Component {
     }
   };
 
-  renderPosts = () => {
-    const { posts, username, profileImg, id } = this.props.FetchUserQuery.user;
+  renderPosts = (fetchedUser, currentUser) => {
+    const { posts, username, profileImg, id } = fetchedUser;
 
     if (posts.length === 0) {
       return (
@@ -44,7 +44,7 @@ class User extends Component {
             <Post
               post={post}
               profile={{ username, profileImg, id }}
-              me={this.props.CurrentUserQuery.me}
+              me={currentUser.me}
             />
           </div>
         );
@@ -53,84 +53,84 @@ class User extends Component {
   };
 
   render() {
-    const { CurrentUserQuery, FetchUserQuery, history, match } = this.props;
-
-    if (CurrentUserQuery.loading || FetchUserQuery.loading) {
-      return <i className="fa fa-spinner fa-pulse fa-3x fa-fw" />;
-    }
-
-    const { user } = FetchUserQuery;
-
-    if (!user) {
-      history.push(`/404/user/${match.params.username}`);
-      return null;
-    }
-
-    const authenticated =
-      CurrentUserQuery.me && user.id === CurrentUserQuery.me.id;
-    const authorized = !!CurrentUserQuery.me;
+    const { history, match } = this.props;
 
     return (
-      <div className="mt-3">
-        <div className="row p-3 m-0">
-          <div className="col-xs-12 col-sm-4">
-            <div className="card">
-              <img
-                src={user.profileImg}
-                alt="header"
-                className="card-img-top"
-              />
-              <div className="card-body">
-                <div>
-                  <b>{user.username}</b>
-                </div>
-                <div>{user.bio}</div>
-                <div>{user.location}</div>
-                {user.isAFoodTruck && (
-                  <div>
-                    <Rater
-                      total={5}
-                      onRate={this.rateUser}
-                      rating={parseFloat(user.rating.average)}
-                      interactive={user.isAFoodTruck && authorized}
-                    />
-                    <p>
-                      <small>
-                        Rated <span>{user.rating.average}</span> out of 5!
-                      </small>
-                    </p>
+      <Query query={FETCH_USER} variables={{ username: match.params.username }}>
+        {({ loading: fetchLoading, error: fetchErr, data: fetchedUser }) => (
+          <Query query={CURRENT_USER} variables={{ withLikedPosts: true }}>
+            {({
+              loading: currentLoading,
+              error: currentErr,
+              data: currentUser,
+            }) => {
+              if (fetchLoading || currentLoading) {
+                return <i className="fa fa-spinner fa-pulse fa-3x fa-fw" />;
+              }
+
+              const { user } = fetchedUser;
+
+              if (!user) {
+                history.push(`/404/user/${match.params.username}`);
+                return;
+              }
+
+              const authenticated =
+                currentUser.me && user.id === currentUser.me.id;
+
+              return (
+                <div className="mt-3">
+                  <div className="row p-3 m-0">
+                    <div className="col-xs-12 col-sm-4">
+                      <div className="card">
+                        <img
+                          src={user.profileImg}
+                          alt="header"
+                          className="card-img-top"
+                        />
+                        <div className="card-body">
+                          <div>
+                            <b>{user.username}</b>
+                          </div>
+                          <div>{user.bio}</div>
+                          <div>{user.location}</div>
+                          {user.isAFoodTruck && (
+                            <div>
+                              <Rater
+                                total={5}
+                                onRate={this.rateUser}
+                                rating={parseFloat(user.rating.average)}
+                                interactive={
+                                  user.isAFoodTruck && !!currentUser.me
+                                }
+                              />
+                              <p>
+                                <small>
+                                  Rated {user.rating.average} out of 5!
+                                </small>
+                              </p>
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-xs-12 col-sm-8">
+                      <div className="card">
+                        {authenticated && (
+                          <div className="card-body">
+                            <Chowt user={user} />
+                          </div>
+                        )}
+                        {this.renderPosts(user, currentUser)}
+                      </div>
+                    </div>
                   </div>
-                )}
-              </div>
-            </div>
-          </div>
-          <div className="col-xs-12 col-sm-8">
-            <div className="card">
-              {authenticated && (
-                <div className="card-body">
-                  <Chowt user={user} />
                 </div>
-              )}
-              {this.renderPosts()}
-            </div>
-          </div>
-        </div>
-      </div>
+              );
+            }}
+          </Query>
+        )}
+      </Query>
     );
   }
 }
-
-export default compose(
-  graphql(FetchUser, {
-    name: 'FetchUserQuery',
-    options: props => ({
-      variables: { username: props.match.params.username },
-    }),
-  }),
-  graphql(CurrentUser, {
-    name: 'CurrentUserQuery',
-    options: props => ({
-      variables: { withLikedPosts: true },
-    }),
-  })
-)(User);
