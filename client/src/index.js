c9515870ea7abe69009b6d9353295e4f8553bcf2
Aaron <aaronmassey45@gmail.com>
2018-11-21 07:55:52 -0600

    Remove redux from app

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import ApolloClient, { InMemoryCache } from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 import App from './App';
-import store from '../src/store/store';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -31,10 +29,8 @@ const client = new ApolloClient({
 });
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 );
