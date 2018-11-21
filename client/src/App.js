622eb49807b7ff878943338978cee24e1db7916b
Aaron <aaronmassey45@gmail.com>
2018-11-21 06:55:40 -0600

    Converted to functional component and removed redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { connect } from 'react-redux';
 
 import requireAuth from './components/HOCs/requireAuth';
 import EditUser from './components/editUser/EditUser';
@@ -12,35 +11,23 @@ import NotFound from './components/NotFound';
 import SignUp from './components/Signup';
 import User from './components/User';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <div className="mt-app">
-            <Switch>
-              <Route path="/login" exact component={Login} />
-              <Route
-                path="/account/edit"
-                exact
-                component={requireAuth(EditUser)}
-              />
-              <Route path="/users/account/:username" exact component={User} />
-              <Route path="/signup" exact component={SignUp} />
-              <Route path="/" exact component={HomePage} />
-              <Route path="*" component={NotFound} />
-            </Switch>
-            {this.props.appState.loggedIn ? <FloatingChowt /> : ''}
-          </div>
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <div className="App">
+      <Navbar />
+      <div className="mt-app">
+        <Switch>
+          <Route path="/login" exact component={Login} />
+          <Route path="/account/edit" exact component={requireAuth(EditUser)} />
+          <Route path="/users/account/:username" exact component={User} />
+          <Route path="/signup" exact component={SignUp} />
+          <Route path="/" exact component={HomePage} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+        <FloatingChowt />
+      </div>
+    </div>
+  </BrowserRouter>
+);
 
-const mapStateToProps = state => ({
-  appState: state.appState,
-});
-
-export default connect(mapStateToProps)(App);
+export default App;
