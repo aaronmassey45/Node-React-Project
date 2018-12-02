89e60b7b726355a428067f2fabbc47e31c9483ac
Aaron <aaronmassey45@gmail.com>
2018-12-02 12:53:24 -0600

    Basic landing page

---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,28 @@
+import React, { PureComponent } from 'react';
+import { Link } from 'react-router-dom';
+
+class LandingPage extends PureComponent {
+  render() {
+    return (
+      <div className="landing-page text-white row container mx-auto">
+        <div id="welcome" className="col-12 ubuntu">
+          Welcome to <div className="chowster-font display-3">Chowster</div>
+        </div>
+        <div className="col-6">
+          Not a member?
+          <div>
+            <Link to="/signup">Sign up!</Link>
+          </div>
+        </div>
+        <div className="col-6">
+          Have an account?
+          <div>
+            <Link to="/login">Log in!</Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
