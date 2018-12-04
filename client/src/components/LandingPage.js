5b543007234eda24638fb0a03afc888f928e9b91
Aaron <aaronmassey45@gmail.com>
2018-12-03 23:55:03 -0600

    Created landing page

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,28 +1,35 @@
-import React, { PureComponent } from 'react';
-import { Link } from 'react-router-dom';
+import React, { memo } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { Query } from 'react-apollo';
 
-class LandingPage extends PureComponent {
-  render() {
-    return (
-      <div className="landing-page text-white row container mx-auto">
-        <div id="welcome" className="col-12 ubuntu">
-          Welcome to <div className="chowster-font display-3">Chowster</div>
-        </div>
-        <div className="col-6">
-          Not a member?
-          <div>
-            <Link to="/signup">Sign up!</Link>
+import CURRENT_USER from '../queries/CurrentUser';
+
+const LandingPage = () => (
+  <Query query={CURRENT_USER}>
+    {({ data, loading }) => {
+      return !loading && data.me ? (
+        <Redirect to="/feed" />
+      ) : (
+        <div className="landing-page text-white row container mx-auto">
+          <div id="welcome" className="col-12 ubuntu">
+            Welcome to <div className="chowster-font display-3">Chowster</div>
+          </div>
+          <div className="col-6">
+            Not a member?
+            <div>
+              <Link to="/signup">Sign up!</Link>
+            </div>
+          </div>
+          <div className="col-6">
+            Have an account?
+            <div>
+              <Link to="/login">Log in!</Link>
+            </div>
           </div>
         </div>
-        <div className="col-6">
-          Have an account?
-          <div>
-            <Link to="/login">Log in!</Link>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+      );
+    }}
+  </Query>
+);
 
 export default LandingPage;
