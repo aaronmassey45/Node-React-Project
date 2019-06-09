10ef3671ed4d587de7a2eacf64fbdcebb4865b6c
Aaron <aaronmassey45@gmail.com>
2019-06-09 07:30:27 -0500

    Changed into functional component

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,111 +1,94 @@
-import React, { Component, Fragment } from 'react';
+import React, { memo, Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import moment from 'moment';
 
 import Alert from './Alert';
 import addAlertProps from './HOCs/add-alert';
 import LikePostButton from './LikePostButton';
 import DeletePostButton from './DeletePostButton';
+import getTimeDifference from '../utils/getTimeDifference';
 
-class Post extends Component {
-  getTimeDifference = time => {
-    const now = moment(new Date().getTime());
-    const createdAt = moment(time);
+const Post = ({
+  alert: { msg, bg },
+  hide,
+  me,
+  post,
+  profile,
+  show,
+  showModal,
+  updateAlert,
+}) => {
+  const timeString = getTimeDifference(new Date(Number(post.timeCreated)));
 
-    return now.diff(createdAt, 'days') >= 30
-      ? moment(time).format('MMM DD, YYYY')
-      : moment(time).fromNow();
-  };
+  const iLiked = me && me.likedPosts.find(postObj => postObj.id === post.id);
 
-  render() {
-    const {
-      alert: { msg, bg },
-      hide,
-      me,
-      post,
-      profile,
-      show,
-      showModal,
-      updateAlert,
-    } = this.props;
-
-    const timeString = this.getTimeDifference(
-      new Date(Number(post.timeCreated))
-    );
-
-    const iLiked = me && me.likedPosts.find(postObj => postObj.id === post.id);
-
-    return (
-      <Fragment>
-        {showModal && <Alert closeModal={hide} msg={msg} bg={bg} />}
-        <div className="Post">
-          <div className="media">
-            <img
-              src={profile.profileImg}
-              alt="user avatar"
-              className="rounded mx-3 img-fluid"
-            />
-            <div className="media-body container">
-              <div className="my-auto itim-font">
-                <div className="text-left row">
-                  <span className="col-10">
-                    <Link to={`/users/account/${profile.username}`}>
-                      @{profile.username}
-                    </Link>
-                  </span>
-                  {me && profile.id === me.id && (
-                    <span className="col-2 text-right">
-                      <DeletePostButton
-                        id={post.id}
-                        show={show}
-                        updateAlert={updateAlert}
-                        username={profile.username}
-                      />
-                    </span>
-                  )}
-                </div>
-                <div className="row text-left">
-                  <div className="col-12 mt-1">
-                    {post.text}
-                    {post.location && post.location.lat && (
-                      <p className="mb-0">
-                        <small>
-                          <a
-                            href={`https://www.google.com/maps?q=${
-                              post.location.lat
-                            },${post.location.lng}`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            My Location
-                          </a>
-                        </small>
-                      </p>
-                    )}
-                  </div>
-                  <div className="col-4 mt-1">
-                    <LikePostButton
+  return (
+    <Fragment>
+      {showModal && <Alert closeModal={hide} msg={msg} bg={bg} />}
+      <div className="Post">
+        <div className="media">
+          <img
+            src={profile.profileImg}
+            alt="user avatar"
+            className="rounded mx-3 img-fluid"
+          />
+          <div className="media-body container">
+            <div className="my-auto itim-font">
+              <div className="text-left row">
+                <span className="col-10">
+                  <Link to={`/users/account/${profile.username}`}>
+                    @{profile.username}
+                  </Link>
+                </span>
+                {me && profile.id === me.id && (
+                  <span className="col-2 text-right">
+                    <DeletePostButton
                       id={post.id}
-                      liked={iLiked}
                       show={show}
                       updateAlert={updateAlert}
                       username={profile.username}
                     />
-                    <span className="text-gray ml-2">
-                      {post.likedBy.length}
-                    </span>
-                  </div>
-                  <div className="col-8 mt-1 text-right">
-                    <small className="text-gray">{timeString}</small>
-                  </div>
+                  </span>
+                )}
+              </div>
+              <div className="row text-left">
+                <div className="col-12 mt-1">
+                  {post.text}
+                  {post.location && post.location.lat && (
+                    <p className="mb-0">
+                      <small>
+                        <a
+                          href={`https://www.google.com/maps?q=${
+                            post.location.lat
+                          },${post.location.lng}`}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          My Location
+                        </a>
+                      </small>
+                    </p>
+                  )}
+                </div>
+                <div className="col-4 mt-1">
+                  <LikePostButton
+                    id={post.id}
+                    liked={iLiked}
+                    show={show}
+                    updateAlert={updateAlert}
+                    username={profile.username}
+                  />
+                  <span className="text-gray ml-2">{post.likedBy.length}</span>
+                </div>
+                <div className="col-8 mt-1 text-right">
+                  <small className="text-gray">{timeString}</small>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </Fragment>
-    );
-  }
-}
+      </div>
+    </Fragment>
+  );
+};
 
-export default addAlertProps(Post);
+export default addAlertProps(memo(Post));
