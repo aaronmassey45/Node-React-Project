f51c1dc335385112634e386f5cfdfdeb11742b1c
Aaron <aaronmassey45@gmail.com>
2019-07-23 22:46:54 -0500

    Use Spinner when loading

--- client/src/components/AuthForms/AuthForm.js
+++ client/src/components/AuthForms/AuthForm.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Mutation, Query } from 'react-apollo';
 import { Redirect } from 'react-router-dom';
 
+import Spinner from '../spinner/Spinner';
+
 import SIGNUP from '../../mutations/Signup';
 import LOGIN from '../../mutations/Login';
 import CURRENT_USER_QUERY from '../../queries/CurrentUser';
@@ -51,7 +53,7 @@ export default class AuthForm extends Component {
       <Query query={CURRENT_USER_QUERY}>
         {({ data, refetch, loading }) => {
           if (loading) {
-            return <i className="fa fa-spinner fa-pulse fa-3x fa-fw" />;
+            return <Spinner />;
           }
 
           return data.me ? (
@@ -64,97 +66,104 @@ export default class AuthForm extends Component {
                 onCompleted={data => this._onCompleted(data, refetch)}
                 onError={this.handleError}
               >
-                {mutation => (
-                  <form
-                    onSubmit={e => {
-                      e.preventDefault();
-                      mutation();
-                    }}
-                  >
-                    {isSignup && (
-                      <div className="form-group">
-                        <label htmlFor="email">Email Address</label>
-                        <input
-                          aria-describedby="emailHelp"
-                          className="form-control"
-                          id="email"
-                          onChange={this.handleChange}
-                          placeholder="Enter email"
-                          type="email"
-                          value={email}
-                        />
-                        <small
-                          id="emailHelp"
-                          className="form-text text-muted text-left"
-                        >
-                          We'll never share your email with anyone else.
-                        </small>
-                      </div>
-                    )}
-                    <div className="form-group">
-                      <label htmlFor="username">Username</label>
-                      <input
-                        autoComplete="username"
-                        className="form-control"
-                        id="username"
-                        onChange={this.handleChange}
-                        placeholder="Enter username"
-                        required
-                        type="text"
-                        value={username}
-                      />
+                {(mutation, { loading: loadingMutation }) => {
+                  if (loadingMutation) return <Spinner />;
+
+                  return (
+                    <form
+                      onSubmit={e => {
+                        e.preventDefault();
+                        mutation();
+                      }}
+                    >
                       {isSignup && (
-                        <div className="row" id="userHelp">
-                          <div className="col-6 text-left">
-                            <small className="form-text text-muted">
-                              Min: 4
-                            </small>
-                          </div>
-                          <div className="col-6 text-right">
-                            <small className="form-text text-muted">
-                              Max: 20
-                            </small>
-                          </div>
+                        <div className="form-group">
+                          <label htmlFor="email">Email Address</label>
+                          <input
+                            aria-describedby="emailHelp"
+                            className="form-control"
+                            id="email"
+                            onChange={this.handleChange}
+                            placeholder="Enter email"
+                            type="email"
+                            value={email}
+                          />
+                          <small
+                            id="emailHelp"
+                            className="form-text text-muted text-left"
+                          >
+                            We'll never share your email with anyone else.
+                          </small>
                         </div>
                       )}
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="password">Password</label>
-                      <input
-                        autoComplete="password"
-                        className="form-control"
-                        id="password"
-                        onChange={this.handleChange}
-                        placeholder="Password"
-                        required
-                        type="password"
-                        value={password}
-                      />
-                      {isSignup && (
-                        <small className="form-text text-muted text-left">
-                          Min: 6
-                        </small>
-                      )}
-                    </div>
-                    {isSignup && (
-                      <div className="form-check my-2">
-                        <label className="form-check-label">
-                          <input
-                            checked={isAFoodTruck}
-                            className="form-check-input"
-                            id="isAFoodTruck"
-                            onChange={this.handleChange}
-                            type="checkbox"
-                          />
-                          Is this a food truck account?
-                        </label>
+                      <div className="form-group">
+                        <label htmlFor="username">Username</label>
+                        <input
+                          autoComplete="username"
+                          className="form-control"
+                          id="username"
+                          onChange={this.handleChange}
+                          placeholder="Enter username"
+                          required
+                          type="text"
+                          value={username}
+                        />
+                        {isSignup && (
+                          <div className="row" id="userHelp">
+                            <div className="col-6 text-left">
+                              <small className="form-text text-muted">
+                                Min: 4
+                              </small>
+                            </div>
+                            <div className="col-6 text-right">
+                              <small className="form-text text-muted">
+                                Max: 20
+                              </small>
+                            </div>
+                          </div>
+                        )}
                       </div>
-                    )}
-                    <button className="btn btn-primary btn-block" type="submit">
-                      {isSignup ? 'Create Account' : 'Login!'}
-                    </button>
-                  </form>
-                )}
+                      <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input
+                          autoComplete="password"
+                          className="form-control"
+                          id="password"
+                          onChange={this.handleChange}
+                          placeholder="Password"
+                          required
+                          type="password"
+                          value={password}
+                        />
+                        {isSignup && (
+                          <small className="form-text text-muted text-left">
+                            Min: 6
+                          </small>
+                        )}
+                      </div>
+                      {isSignup && (
+                        <div className="form-check my-2">
+                          <label className="form-check-label">
+                            <input
+                              checked={isAFoodTruck}
+                              className="form-check-input"
+                              id="isAFoodTruck"
+                              onChange={this.handleChange}
+                              type="checkbox"
+                            />
+                            Is this a food truck account?
+                          </label>
+                        </div>
+                      )}
+                      <button
+                        className="btn btn-primary btn-block"
+                        type="submit"
+                      >
+                        {isSignup ? 'Create Account' : 'Login!'}
+                      </button>
+                    </form>
+                  );
+                }}
               </Mutation>
               {hasErr && (
                 <div className="alert alert-danger mb-0 mt-2" role="alert">
