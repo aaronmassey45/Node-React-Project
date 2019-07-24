3a19b4e9a25e975450f34721e16ddbfee6548caf
Aaron <aaronmassey45@gmail.com>
2019-07-24 08:19:06 -0500

    Initialize state based on whether you are following Pass in mutation to follow user

--- client/src/components/follow-button/FollowButton.js
+++ client/src/components/follow-button/FollowButton.js
@@ -1,30 +1,41 @@
 import React, { useState, Fragment } from 'react';
+import { Mutation } from 'react-apollo';
 import classNames from 'classnames';
+
+import FOLLOW_USER from '../../mutations/FollowUser';
 
 import './follow-button.styles.scss';
 
-const FollowButton = () => {
-  const [following, setFollowingState] = useState(false);
+const FollowButton = ({ following, userId }) => {
+  const [isFollowing, setFollowingState] = useState(following);
   const btnClasses = classNames('follow-button', 'btn', {
-    'btn-success': following,
-    'btn-primary': !following,
+    'btn-success': isFollowing,
+    'btn-primary': !isFollowing,
   });
 
   return (
-    <button
-      className={btnClasses}
-      onClick={() => setFollowingState(!following)}
+    <Mutation
+      mutation={FOLLOW_USER}
+      variables={{ id: userId }}
+      onCompleted={() => setFollowingState(true)}
+      onError={err => console.log(err)}
     >
-      {following ? (
-        <Fragment>
-          Following <i className="fas fa-check"></i>
-        </Fragment>
-      ) : (
-        <Fragment>
-          Follow <i className="fas fa-user-plus"></i>
-        </Fragment>
-      )}
-    </button>
+      {(followUser, { loading }) => {
+        return (
+          <button className={btnClasses} onClick={followUser}>
+            {isFollowing ? (
+              <Fragment>
+                Following <i className="fas fa-check"></i>
+              </Fragment>
+            ) : (
+              <Fragment>
+                Follow <i className="fas fa-user-plus"></i>
+              </Fragment>
+            )}
+          </button>
+        );
+      }}
+    </Mutation>
   );
 };
 
