eed2b24fb50d7e5f28a7ac8742d4139dc2fcebef
Aaron <aaronmassey45@gmail.com>
2019-07-23 22:36:35 -0500

    Updated import for NotFound Render Spinner when loading a lazy loaded page Changed from component on Routes to render

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,12 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { default as Navbar } from './components/navbar/NavbarContainer';
 import ProtectedRoute from './components/HOCs/ProtectedRoute';
 import FloatingChowt from './components/ChowtModal';
+import Spinner from './components/spinner/Spinner';
 const EditUser = lazy(() => import('./components/editUser/EditUser'));
 const HomePage = lazy(() => import('./components/Homepage'));
 const LandingPage = lazy(() => import('./components/LandingPage'));
 const Login = lazy(() => import('./components/AuthForms/Login'));
-const NotFound = lazy(() => import('./components/NotFound'));
+const NotFound = lazy(() => import('./pages/not-found/NotFound'));
 const SignUp = lazy(() => import('./components/AuthForms/Signup'));
 const User = lazy(() => import('./components/User'));
 
@@ -17,40 +18,36 @@ const App = () => (
     <div className="App">
       <Navbar />
       <div className="mt-app">
-        <Suspense fallback={<div />}>
+        <Suspense fallback={<Spinner />}>
           <Switch>
-            <Route
-              path="/login"
-              exact
-              component={props => <Login {...props} />}
-            />
+            <Route path="/login" exact render={props => <Login {...props} />} />
             <ProtectedRoute
               path="/account/edit"
               exact
-              component={props => <EditUser {...props} />}
+              render={props => <EditUser {...props} />}
             />
             <Route
               path="/users/account/:username"
               exact
-              component={props => <User {...props} />}
+              render={props => <User {...props} />}
             />
             <Route
               path="/signup"
               exact
-              component={props => <SignUp {...props} />}
+              render={props => <SignUp {...props} />}
             />
             <ProtectedRoute
               path="/feed"
               redirectTo="/"
               exact
-              component={props => <HomePage {...props} />}
+              render={props => <HomePage {...props} />}
             />
             <Route
               path="/"
               exact
-              component={props => <LandingPage {...props} />}
+              render={props => <LandingPage {...props} />}
             />
-            <Route component={props => <NotFound {...props} />} />
+            <Route render={props => <NotFound {...props} />} />
           </Switch>
         </Suspense>
         <FloatingChowt />
