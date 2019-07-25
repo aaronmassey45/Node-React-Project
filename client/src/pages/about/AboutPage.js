115660e91967d93ba6f81175fccc4fe4cc113e93
Aaron <aaronmassey45@gmail.com>
2019-07-25 08:59:43 -0500

    Created about page

---
+++ client/src/pages/about/AboutPage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './about.styles.scss';
+
+const AboutPage = () => {
+  return (
+    <div id="about">
+      <div className="card">
+        <div className="card-body">
+          <h1>About Us</h1>
+          <div className="dont-sue-me">
+            I am in no way affiliated with Bob's Burgers. This is just a project
+            for me to practice my web development skills. Enjoy yourself and
+            don't get me sued!
+          </div>
+          <img
+            src="https://vignette.wikia.nocookie.net/bobsburgerpedia/images/b/b7/Gene_Belcher.png/revision/latest?cb=20130114043308"
+            className="responsive-img"
+            alt="Burger Boy"
+          />
+          <div className="content">
+            Chowster serves all users who are passionate about food trucks! Our
+            aim is to provide a safe haven for food truckers and truckees to
+            connect with each other and do whatever weird things you guys do!
+            Probably a lot of food fetishes out there. Don't be gross on here...
+            Just mingle and have fun. But not too much fun.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
