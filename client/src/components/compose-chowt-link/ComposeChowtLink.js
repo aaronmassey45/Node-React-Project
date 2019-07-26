2195deac48ba5bebcf13c4c0779e8b7a0f41da5a
Aaron <aaronmassey45@gmail.com>
2019-07-25 21:21:22 -0500

    Renamed ChowtModal to ComposeChowtLink and moved into its own folder Moved styles for button from App.css into its own file

---
+++ client/src/components/compose-chowt-link/ComposeChowtLink.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Query } from 'react-apollo';
+import { Link } from 'react-router-dom';
+
+import CURRENT_USER from '../../queries/CurrentUser';
+
+import './compose-chowt-link.styles.scss';
+
+const ComposeChowtLink = () => (
+  <Query query={CURRENT_USER} variables={{ withEditingData: true }}>
+    {({ loading, data }) => {
+      if (loading || (!loading && !data.me)) return null;
+
+      return (
+        <div id="compose-chowt-link">
+          <Link className="btn btn-gray fake-link" to="/compose/chowt">
+            <i class="fas fa-feather-alt" />
+          </Link>
+        </div>
+      );
+    }}
+  </Query>
+);
+
+export default ComposeChowtLink;
