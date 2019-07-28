ec90707229bb962cb0a9407c86ba7f02a390cb2c
Aaron <aaronmassey45@gmail.com>
2019-07-28 09:23:48 -0500

    import and use Spinner and home-page.styles.scss

--- client/src/pages/home-page/Homepage.js
+++ client/src/pages/home-page/Homepage.js
@@ -1,44 +1,43 @@
 import React from 'react';
 import { Query } from 'react-apollo';
 
-import Post from './Post';
-import CurrentUser from '../queries/CurrentUser';
-import GetPosts from '../queries/GetPosts';
+import Post from './../../components/Post';
+import CurrentUser from '../../queries/CurrentUser';
+import GetPosts from '../../queries/GetPosts';
+import Spinner from '../../components/spinner/Spinner';
 
-const HomePage = () => {
-  return (
-    <Query query={GetPosts}>
-      {({ loading: loadingOne, data: { posts } }) => (
-        <Query query={CurrentUser} variables={{ withLikedPosts: true }}>
-          {({ loading: loadingTwo, data: { me } }) => (
-            <div className="HomePage container mb-2">
-              <h1 className="chowster-font">Chowster</h1>
-              <div className="card">
-                <div className="card-header">Most Recent Posts</div>
-                {loadingOne || loadingTwo ? (
-                  <div className="card-body">
-                    <div className="text-center">
-                      <i className="fa fa-spinner fa-pulse fa-3x fa-fw" />
-                    </div>
-                  </div>
-                ) : (
-                  posts
-                    .map(post => {
-                      return (
-                        <div key={post.id} className="list-group-item">
-                          <Post post={post} profile={post._creator} me={me} />
-                        </div>
-                      );
-                    })
-                    .reverse()
-                )}
-              </div>
+import './home-page.styles.scss';
+
+const HomePage = () => (
+  <Query query={GetPosts}>
+    {({ loading: loadingOne, data: { posts } }) => (
+      <Query query={CurrentUser} variables={{ withLikedPosts: true }}>
+        {({ loading: loadingTwo, data: { me } }) => (
+          <div className="HomePage container mb-2">
+            <h1 className="chowster-font">Chowster</h1>
+            <div className="card">
+              <div className="card-header">Most Recent Posts</div>
+              {loadingOne || loadingTwo ? (
+                <div className="list-group-item">
+                  <Spinner />
+                </div>
+              ) : (
+                posts
+                  .map(post => {
+                    return (
+                      <div key={post.id} className="list-group-item">
+                        <Post post={post} profile={post._creator} me={me} />
+                      </div>
+                    );
+                  })
+                  .reverse()
+              )}
             </div>
-          )}
-        </Query>
-      )}
-    </Query>
-  );
-};
+          </div>
+        )}
+      </Query>
+    )}
+  </Query>
+);
 
 export default HomePage;
