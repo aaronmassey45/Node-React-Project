e2fc206349ac324d2769d2ce0e42249f98391afc
Aaron <aaronmassey45@gmail.com>
2019-07-29 20:31:07 -0500

    Remove query

--- client/src/components/compose-chowt-link/ComposeChowtLink.js
+++ client/src/components/compose-chowt-link/ComposeChowtLink.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import { Query } from 'react-apollo';
 import { Link } from 'react-router-dom';
-
-import CURRENT_USER from '../../queries/CurrentUser';
 
 import './compose-chowt-link.styles.scss';
 
 const ComposeChowtLink = () => (
-  <Query query={CURRENT_USER} variables={{ withEditingData: true }}>
-    {({ loading, data }) => {
-      if (loading || (!loading && !data.me)) return null;
-
-      return (
-        <div id="compose-chowt-link">
-          <Link className="btn btn-gray fake-link" to="/compose/chowt">
-            <i className="fas fa-bullhorn" />
-          </Link>
-        </div>
-      );
-    }}
-  </Query>
+  <div id="compose-chowt-link">
+    <Link className="btn btn-gray fake-link" to="/compose/chowt">
+      <i className="fas fa-bullhorn" />
+    </Link>
+  </div>
 );
 
 export default ComposeChowtLink;
