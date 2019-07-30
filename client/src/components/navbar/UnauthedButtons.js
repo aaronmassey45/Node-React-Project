212edb980f2be64ea0dde48c32440d9f693d58bd
Aaron <aaronmassey45@gmail.com>
2019-07-29 20:37:27 -0500

    Added conditionals on when to render text Added prop-types

--- client/src/components/navbar/UnauthedButtons.js
+++ client/src/components/navbar/UnauthedButtons.js
@@ -1,29 +1,27 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 
-export default () => {
+const UnauthedButtons = ({ renderIconsOnly }) => {
   return (
     <Fragment>
-      <li
-        className="nav-item"
-        data-toggle="collapse"
-        data-target=".navbar-collapse.show"
-      >
+      <li>
         <NavLink to="/login" exact className="nav-link">
-          <i className="fa fa-sign-in fa-fw" />
-          Login
+          <i className="fa fa-sign-in fa-fw" /> {!renderIconsOnly && 'Login'}
         </NavLink>
       </li>
-      <li
-        className="nav-item"
-        data-toggle="collapse"
-        data-target=".navbar-collapse.show"
-      >
+      <li>
         <NavLink to="/signup" exact className="nav-link">
-          <i className="fa fa-user-plus fa-fw" />
-          Sign Up
+          <i className="fa fa-user-plus fa-fw" />{' '}
+          {!renderIconsOnly && 'Sign Up'}
         </NavLink>
       </li>
     </Fragment>
   );
 };
+
+UnauthedButtons.propTypes = {
+  renderIconsOnly: PropTypes.bool.isRequired,
+};
+
+export default UnauthedButtons;
