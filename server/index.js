e789ec500d10361099b2da86d395bc17f2e0af7b
Aaron <aaronmassey45@gmail.com>
2019-08-02 08:05:17 -0500

    Updated mongoose.connect to get rid of deprecation warning

--- server/index.js
+++ server/index.js
@@ -9,7 +9,10 @@ const schema = require('./schema/schema');
 const authenticate = require('./middleware/authenticate');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
 
 const app = express();
 
