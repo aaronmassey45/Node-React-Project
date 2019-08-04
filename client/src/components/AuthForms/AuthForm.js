a9f9b7cee74c7adbe8b0ed375dc6e1df7a81b40e
Aaron <aaronmassey45@gmail.com>
2019-08-03 21:19:23 -0500

    Remove Query component and utilize FormInput

--- client/src/components/AuthForms/AuthForm.js
+++ client/src/components/AuthForms/AuthForm.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { Mutation, Query } from 'react-apollo';
-import { Redirect } from 'react-router-dom';
+import { Mutation } from 'react-apollo';
+import PropTypes from 'prop-types';
 
 import Spinner from '../spinner/Spinner';
 import FormInput from '../form-input/FormInput';
 
 import SIGNUP from '../../mutations/Signup';
 import LOGIN from '../../mutations/Login';
-import CURRENT_USER_QUERY from '../../queries/CurrentUser';
 
 export default class AuthForm extends Component {
   state = {
@@ -24,10 +23,10 @@ export default class AuthForm extends Component {
     this.setState({ [id]: id === 'isAFoodTruck' ? checked : value });
   };
 
-  _onCompleted = (data, refetch) => {
+  _onCompleted = data => {
     const token = this.props.isSignup ? data.signup : data.login;
     localStorage.setItem('x-auth', token);
-    refetch();
+    this.props.refetch();
   };
 
   handleError = err => {
@@ -64,147 +63,112 @@ export default class AuthForm extends Component {
     const { isSignup } = this.props;
 
     return (
-      <Query query={CURRENT_USER_QUERY}>
-        {({ data, refetch, loading }) => {
-          if (loading) {
-            return <Spinner />;
-          }
+      <>
+        <Mutation
+          mutation={isSignup ? SIGNUP : LOGIN}
+          variables={{ email, password, username, isAFoodTruck }}
+          onCompleted={data => this._onCompleted(data)}
+          onError={this.handleError}
+        >
+          {(mutation, { loading: loadingMutation }) => {
+            if (loadingMutation) return <Spinner />;
 
-          return data.me ? (
-            <Redirect to={`/users/account/${data.me.username}`} />
-          ) : (
-            <>
-              <Mutation
-                mutation={isSignup ? SIGNUP : LOGIN}
-                variables={{ email, password, username, isAFoodTruck }}
-                onCompleted={data => this._onCompleted(data, refetch)}
-                onError={this.handleError}
-              >
-                {(mutation, { loading: loadingMutation }) => {
-                  if (loadingMutation) return <Spinner />;
-
-                  return (
-                    <form
-                      onSubmit={e => this.handleSubmit(e, mutation, isSignup)}
-                    >
-                      {isSignup && (
-                        <div className="form-group">
-                          <label htmlFor="email">Email Address</label>
-                          <input
-                            aria-describedby="emailHelp"
-                            className="form-control"
-                            id="email"
-                            onChange={this.handleChange}
-                            placeholder="Enter email"
-                            type="email"
-                            value={email}
-                          />
-                          <small
-                            id="emailHelp"
-                            className="form-text text-muted text-left"
-                          >
-                            We'll never share your email with anyone else.
-                          </small>
-                        </div>
-                      )}
-                      <div className="form-group">
-                        <label htmlFor="username">Username</label>
-                        <input
-                          autoComplete="username"
-                          className="form-control"
-                          id="username"
-                          onChange={this.handleChange}
-                          placeholder="Enter username"
-                          required
-                          type="text"
-                          value={username}
-                        />
-                        {isSignup && (
-                          <div className="row" id="userHelp">
-                            <div className="col-6 text-left">
-                              <small className="form-text text-muted">
-                                Min: 4
-                              </small>
-                            </div>
-                            <div className="col-6 text-right">
-                              <small className="form-text text-muted">
-                                Max: 20
-                              </small>
-                            </div>
-                          </div>
-                        )}
-                      </div>
-                      <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <input
-                          autoComplete="password"
-                          className="form-control"
-                          id="password"
-                          onChange={this.handleChange}
-                          placeholder="Password"
-                          required
-                          type="password"
-                          value={password}
-                        />
-                      </div>
-                      {isSignup && (
-                        <div className="form-group">
-                          <label htmlFor="confirm-password">Password</label>
-                          <input
-                            autoComplete="password"
-                            className="form-control"
-                            id="confirmPassword"
-                            onChange={this.handleChange}
-                            placeholder="Confirm Password"
-                            required
-                            type="password"
-                            value={confirmPassword}
-                          />
-                          <small className="form-text text-muted text-left">
-                            Min: 6
-                          </small>
-                        </div>
-                      )}
-                      {isSignup && (
-                        <div className="form-check my-2">
-                          <label className="form-check-label">
-                            <input
-                              checked={isAFoodTruck}
-                              className="form-check-input"
-                              id="isAFoodTruck"
-                              onChange={this.handleChange}
-                              type="checkbox"
-                            />
-                            Is this a food truck account?
-                          </label>
-                        </div>
-                      )}
-                      <button
-                        className="btn btn-primary btn-block"
-                        type="submit"
-                      >
-                        {isSignup ? 'Create Account' : 'Login!'}
-                      </button>
-                    </form>
-                  );
-                }}
-              </Mutation>
-              {hasErr && (
-                <div className="alert alert-danger mb-0 mt-2" role="alert">
-                  <small>Username or password incorrect.</small>
-                </div>
-              )}
-              {errors && (
-                <div className="alert alert-danger" role="alert">
-                  {errors.map(err => (
-                    <div key={err}>{err}</div>
-                  ))}
-                </div>
-              )}
-            </>
-          );
-        }}
-      </Query>
+            return (
+              <form onSubmit={e => this.handleSubmit(e, mutation, isSignup)}>
+                {isSignup && (
+                  <FormInput
+                    id="email"
+                    onChange={this.handleChange}
+                    placeholder="Enter email"
+                    type="email"
+                    value={email}
+                    label="Email Address"
+                    smallText="We'll never share your email with anyone else."
+                  />
+                )}
+                <FormInput
+                  autoComplete="username"
+                  className="form-control"
+                  id="username"
+                  onChange={this.handleChange}
+                  placeholder="Enter username"
+                  required
+                  type="text"
+                  value={username}
+                  label="Username"
+                  smallText={
+                    isSignup && (
+                      <>
+                        <span>Min: 4</span>{' '}
+                        <span className="right">Max: 20</span>
+                      </>
+                    )
+                  }
+                />
+                <FormInput
+                  autoComplete="password"
+                  className="form-control"
+                  id="password"
+                  onChange={this.handleChange}
+                  placeholder="Password"
+                  required
+                  type="password"
+                  value={password}
+                  label="Password"
+                />
+                {isSignup && (
+                  <FormInput
+                    autoComplete="password"
+                    className="form-control"
+                    id="confirmPassword"
+                    onChange={this.handleChange}
+                    placeholder="Confirm Password"
+                    required
+                    type="password"
+                    value={confirmPassword}
+                    label="Confirm Password"
+                    smallText="Min: 6"
+                  />
+                )}
+                {isSignup && (
+                  <div className="form-check my-2">
+                    <label className="form-check-label">
+                      <input
+                        checked={isAFoodTruck}
+                        className="form-check-input"
+                        id="isAFoodTruck"
+                        onChange={this.handleChange}
+                        type="checkbox"
+                      />
+                      Is this a food truck account?
+                    </label>
+                  </div>
+                )}
+                <button className="btn btn-primary btn-block" type="submit">
+                  {isSignup ? 'Create Account' : 'Login!'}
+                </button>
+              </form>
+            );
+          }}
+        </Mutation>
+        {hasErr && (
+          <div className="alert alert-danger mb-0 mt-2" role="alert">
+            <small>Username or password incorrect.</small>
+          </div>
+        )}
+        {errors && (
+          <div className="alert alert-danger" role="alert">
+            {errors.map(err => (
+              <div key={err}>{err}</div>
+            ))}
+          </div>
+        )}
+      </>
     );
   }
 }
+
+AuthForm.propTypes = {
+  refetch: PropTypes.func.isRequired,
+  isSignup: PropTypes.bool.isRequired,
+};
