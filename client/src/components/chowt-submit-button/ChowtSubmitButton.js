d4ba4b39357195a64ca7d7125c9e116b94264c29
Aaron <aaronmassey45@gmail.com>
2019-08-05 13:15:05 -0500

    Handles cache update without fetching from db

--- client/src/components/chowt-submit-button/ChowtSubmitButton.js
+++ client/src/components/chowt-submit-button/ChowtSubmitButton.js
@@ -1,33 +1,64 @@
 import React from 'react';
-import { Mutation } from 'react-apollo';
+import { Mutation, withApollo } from 'react-apollo';
 
 import Spinner from '../../components/spinner/Spinner';
 
 import SEND_CHOWT from '../../mutations/Chowt';
-import GET_POSTS from '../../queries/GetPosts';
+import GET_USERS_FEED from '../../queries/getUsersFeed';
+import FETCH_USER from '../../queries/FetchUser';
 
 import './chowt-submit-button.styles.scss';
 
-const ChowtSubmitButton = ({ history, disabled, handleSubmit }) => {
-  return (
-    <Mutation
-      mutation={SEND_CHOWT}
-      onCompleted={history.goBack}
-      refetchQueries={[{ query: GET_POSTS }]}
-      awaitRefetchQueries
-    >
-      {(sendChowt, { loading }) => (
-        <button
-          id="chowt-submit-button"
-          className="btn"
-          onClick={() => handleSubmit(sendChowt)}
-          disabled={disabled}
-        >
-          {loading && <Spinner />} Chowt
-        </button>
-      )}
-    </Mutation>
-  );
+const ChowtSubmitButton = ({ history, disabled, handleSubmit, client }) => (
+  <Mutation
+    mutation={SEND_CHOWT}
+    onCompleted={history.goBack}
+    update={(proxy, { data: { chowt } }) => onUpdate(proxy, chowt, client)}
+  >
+    {(sendChowt, { loading }) => (
+      <button
+        id="chowt-submit-button"
+        className="btn"
+        onClick={() => handleSubmit(sendChowt)}
+        disabled={disabled}
+      >
+        {loading && <Spinner />} Chowt
+      </button>
+    )}
+  </Mutation>
+);
+
+const onUpdate = (proxy, chowt, client) => {
+  try {
+    const userId = client.extract().ROOT_QUERY.me.id;
+    const username = client.extract()[userId].username;
+
+    const { user } = proxy.readQuery({
+      query: FETCH_USER,
+      variables: { username },
+    });
+
+    proxy.writeQuery({
+      query: FETCH_USER,
+      variables: { username },
+      data: {
+        user: { ...user, posts: [...user.posts.reverse(), chowt] },
+      },
+    });
+  } catch (err) {
+    console.log('Error updating FETCH_USER');
+  }
+
+  try {
+    const { populateFeed } = proxy.readQuery({ query: GET_USERS_FEED });
+
+    proxy.writeQuery({
+      query: GET_USERS_FEED,
+      data: { populateFeed: [chowt, ...populateFeed] },
+    });
+  } catch (err) {
+    console.log('Error updating GET_USERS_FEED');
+  }
 };
 
-export default ChowtSubmitButton;
+export default withApollo(ChowtSubmitButton);
