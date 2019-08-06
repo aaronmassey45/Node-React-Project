01348b045e72128d22271761a5a58c5ff02138b1
Aaron <aaronmassey45@gmail.com>
2019-08-06 12:57:20 -0500

    Get necessary data using Query component

--- client/src/components/editUser/EditUser.js
+++ client/src/components/editUser/EditUser.js
@@ -1,31 +1,27 @@
 import React, { PureComponent } from 'react';
-import { graphql, compose } from 'react-apollo';
+import { graphql, compose, Query } from 'react-apollo';
 
 import Alert from '../Alert';
+import Spinner from '../spinner/Spinner';
 import addAlertProps from '../HOCs/add-alert';
 import InputField from './InputField';
 import DeleteAccount from './DeleteAccount';
 import mutation from '../../mutations/UpdateUser';
 import FIELDS from './form-fields';
+import CURRENT_USER from '../../queries/CurrentUser';
 
 class AccountEdit extends PureComponent {
-  constructor(props) {
-    super(props);
-
-    const { user } = props;
-
-    this.state = {
-      bio: user.bio,
-      currentPassword: '',
-      email: user.email,
-      isAFoodTruck: user.isAFoodTruck,
-      errors: {},
-      location: user.location,
-      newPassword: '',
-      profileImg: user.profileImg,
-      username: user.username,
-    };
-  }
+  state = {
+    bio: '',
+    currentPassword: '',
+    email: '',
+    isAFoodTruck: false,
+    errors: {},
+    location: null,
+    newPassword: '',
+    profileImg: '',
+    username: '',
+  };
 
   updateAndShowAlert = ({ bg, msg }) => {
     this.props.updateAlert({ bg, msg });
@@ -146,45 +142,71 @@ class AccountEdit extends PureComponent {
     const { alert, showModal, clearAlert, hide } = this.props;
 
     return (
-      <div className="AccountEdit text-left container my-1">
-        <DeleteAccount />
-        {showModal && (
-          <Alert
-            closeModal={() => {
-              clearAlert();
-              hide();
-            }}
-            msg={alert.msg}
-            bg={alert.bg}
-          />
-        )}
+      <Query
+        query={CURRENT_USER}
+        variables={{ withEditingData: true }}
+        onCompleted={({ me }) => {
+          this.setState(prevState => ({
+            ...prevState,
+            bio: me.bio,
+            email: me.email,
+            isAFoodTruck: me.isAFoodTruck,
+            location: me.location,
+            profileImg: me.profileImg,
+            username: me.username,
+          }));
+        }}
+      >
+        {({ loading }) => {
+          return loading ? (
+            <Spinner />
+          ) : (
+            <div className="AccountEdit text-left container my-1">
+              <DeleteAccount />
+              {showModal && (
+                <Alert
+                  closeModal={() => {
+                    clearAlert();
+                    hide();
+                  }}
+                  msg={alert.msg}
+                  bg={alert.bg}
+                />
+              )}
 
-        <div className="card bg-secondary text-white mb-3">
-          <div className="card-body p-2">
-            <h4 className="card-title mb-0">Edit User Account</h4>
-          </div>
-        </div>
-        <div className="card bg-light">
-          <div className="card-header">Basic Information</div>
-          <div className="card-body">
-            <form onSubmit={e => e.preventDefault()}>
-              {this.renderFields()}
-            </form>
-            <button
-              className="btn btn-danger text-white"
-              data-toggle="modal"
-              data-target="#deleteModal"
-            >
-              <i className="fa fa-ban" aria-hidden="true" /> Delete account
-            </button>
-          </div>
-          <div className="card-footer text-right">
-            <button className="btn btn-success" onClick={this.handleSubmit}>
-              Update
-            </button>
-          </div>
-        </div>
-      </div>
+              <div className="card bg-secondary text-white mb-3">
+                <div className="card-body p-2">
+                  <h4 className="card-title mb-0">Edit User Account</h4>
+                </div>
+              </div>
+              <div className="card bg-light">
+                <div className="card-header">Basic Information</div>
+                <div className="card-body">
+                  <form onSubmit={e => e.preventDefault()}>
+                    {this.renderFields()}
+                  </form>
+                  <button
+                    className="btn btn-danger text-white"
+                    data-toggle="modal"
+                    data-target="#deleteModal"
+                  >
+                    <i className="fa fa-ban" aria-hidden="true" /> Delete
+                    account
+                  </button>
+                </div>
+                <div className="card-footer text-right">
+                  <button
+                    className="btn btn-success"
+                    onClick={this.handleSubmit}
+                  >
+                    Update
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        }}
+      </Query>
     );
   }
 }
