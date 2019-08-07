bd729bd403d24dd28aa343522fa32d380fedec22
Aaron <aaronmassey45@gmail.com>
2019-08-07 13:20:33 -0500

    Moved into new folder

--- client/src/components/edit-user-form/form-fields.js
+++ client/src/components/edit-user-form/form-fields.js
@@ -3,31 +3,37 @@ export default [
     label: 'Username*',
     name: 'username',
     type: 'text',
+    required: true,
   },
   {
     label: 'Email*',
     name: 'email',
     type: 'email',
+    required: true,
   },
   {
     label: 'Location*',
     name: 'location',
     type: 'text',
+    required: true,
   },
   {
     label: 'URL for Profile Pic*',
     name: 'profileImg',
     type: 'text',
+    required: true,
   },
   {
     label: 'Is account a food truck?*',
     name: 'isAFoodTruck',
     type: 'checkbox',
+    required: true,
   },
   {
     label: 'Bio*',
     name: 'bio',
     type: 'textarea',
+    required: true,
   },
   {
     label: 'New Password',
@@ -35,8 +41,9 @@ export default [
     type: 'password',
   },
   {
-    label: 'Current Password',
+    label: 'Current Password*',
     name: 'currentPassword',
     type: 'password',
+    required: true,
   },
 ];
