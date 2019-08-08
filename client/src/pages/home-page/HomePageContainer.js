b2eca438f1c7b31e80be36fa2eef10bd1749ef93
Aaron <aaronmassey45@gmail.com>
2019-08-08 13:18:44 -0500

    Updated to use apollo hooks

--- client/src/pages/home-page/HomePageContainer.js
+++ client/src/pages/home-page/HomePageContainer.js
@@ -1,41 +1,47 @@
 import React from 'react';
-import { Query } from 'react-apollo';
+import { useQuery } from '@apollo/react-hooks';
 
 import HomePage from './Homepage';
 
 import CURRENT_USER from '../../queries/CurrentUser';
 import GET_USERS_FEED from '../../queries/getUsersFeed';
 
-const HomePageContainer = () => (
-  <Query query={GET_USERS_FEED}>
-    {({ loading: loadingOne, data: { populateFeed: posts }, fetchMore }) => (
-      <Query query={CURRENT_USER} variables={{ withLikedPosts: true }}>
-        {({ loading: loadingTwo, data: { me } }) => (
-          <HomePage
-            isLoading={loadingOne || loadingTwo}
-            currentUser={me || {}}
-            posts={posts || []}
-            onLoadMore={() => {
-              fetchMore({
-                variables: {
-                  offset: posts.length,
-                },
-                updateQuery: (prev, { fetchMoreResult }) => {
-                  if (!fetchMoreResult) return prev;
-                  return {
-                    populateFeed: [
-                      ...prev.populateFeed,
-                      ...fetchMoreResult.populateFeed,
-                    ],
-                  };
-                },
-              });
-            }}
-          />
-        )}
-      </Query>
-    )}
-  </Query>
-);
+const HomePageContainer = () => {
+  const {
+    loading: loadingUserFeed,
+    data: { populateFeed: posts },
+    fetchMore: fetchMorePosts,
+  } = useQuery(GET_USERS_FEED);
+  const {
+    loading: loadingCurrentUser,
+    data: { me },
+  } = useQuery(CURRENT_USER, {
+    variables: { withLikedPosts: true },
+  });
+
+  return (
+    <HomePage
+      isLoading={loadingUserFeed || loadingCurrentUser}
+      currentUser={me || {}}
+      posts={posts || []}
+      onLoadMore={() => {
+        fetchMorePosts({
+          variables: {
+            offset: posts.length,
+          },
+          updateQuery: (prev, { fetchMoreResult }) => {
+            if (!fetchMoreResult) return prev;
+            return {
+              populateFeed: [
+                ...prev.populateFeed,
+                ...fetchMoreResult.populateFeed,
+              ],
+            };
+          },
+        });
+      }}
+    />
+  );
+};
 
 export default HomePageContainer;
