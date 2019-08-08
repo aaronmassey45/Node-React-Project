3080049481e250fcae17465a730832838b18190a
Aaron <aaronmassey45@gmail.com>
2019-08-08 06:51:37 -0500

    Updated to use apollo hooks

--- client/src/components/HOCs/UnProtectedRoute.js
+++ client/src/components/HOCs/UnProtectedRoute.js
@@ -1,34 +1,32 @@
 import React from 'react';
-import { Query } from 'react-apollo';
+import { useQuery } from '@apollo/react-hooks';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import Spinner from '../spinner/Spinner';
 
-import query from '../../queries/CurrentUser';
+import CURRENT_USER from '../../queries/CurrentUser';
 
-const UnProtectedRoute = ({ component: Component, ...rest }) => (
-  <Query query={query}>
-    {({ loading, data, ...queryProps }) => {
-      if (loading) return <Spinner />;
-      return (
-        <Route
-          {...rest}
-          render={props =>
-            data.me ? (
-              <Redirect to={{ pathname: '/home' }} />
-            ) : (
-              <Component {...props} {...queryProps} />
-            )
-          }
-        />
-      );
-    }}
-  </Query>
-);
+const UnProtectedRoute = ({ component: Component, ...rest }) => {
+  const { loading, data, ...queryProps } = useQuery(CURRENT_USER);
+
+  if (loading) return <Spinner />;
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        data.me ? (
+          <Redirect to={{ pathname: '/home' }} />
+        ) : (
+          <Component {...props} {...queryProps} />
+        )
+      }
+    />
+  );
+};
 
 UnProtectedRoute.propTypes = {
-  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func]),
+  component: PropTypes.elementType.isRequired,
 };
 
 export default UnProtectedRoute;
