9a8af643cc1e1ecdbb86acd069c7b93b9712c0fa
Aaron <aaronmassey45@gmail.com>
2019-08-08 06:48:04 -0500

    Added prop-types

--- client/src/components/HOCs/ProtectedRoute.js
+++ client/src/components/HOCs/ProtectedRoute.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import CURRENT_USER from '../../queries/CurrentUser';
 
-const PrivateRoute = ({ component: Component, redirectTo, ...rest }) => {
+const PrivateRoute = ({
+  component: Component,
+  redirectTo = '/login',
+  ...rest
+}) => {
   const { loading, data } = useQuery(CURRENT_USER);
 
   if (loading) return null;
@@ -15,11 +20,16 @@ const PrivateRoute = ({ component: Component, redirectTo, ...rest }) => {
         data.me ? (
           <Component {...props} user={data.me} />
         ) : (
-          <Redirect to={{ pathname: redirectTo || '/login' }} />
+          <Redirect to={{ pathname: redirectTo }} />
         )
       }
     />
   );
+};
+
+PrivateRoute.propTypes = {
+  component: PropTypes.elementType.isRequired,
+  redirectTo: PropTypes.string,
 };
 
 export default PrivateRoute;
