d3c142822b8ede9668df96cdc18da2ae956de79a
Aaron <aaronmassey45@gmail.com>
2019-08-08 07:30:34 -0500

    Updated to use apollo hooks

--- client/src/components/stars/Stars.js
+++ client/src/components/stars/Stars.js
@@ -1,34 +1,30 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Rater from 'react-rater';
-import { Mutation } from 'react-apollo';
+import { useMutation } from '@apollo/react-hooks';
+import PropTypes from 'prop-types';
 
 import RATE_ACCOUNT from '../../mutations/rateAccount';
 
 import './stars.styles.scss';
 
 const Stars = ({ interactive, average, id }) => {
+  const [rateAccount] = useMutation(RATE_ACCOUNT);
+
   return (
     <div id="stars-container">
-      <Mutation mutation={RATE_ACCOUNT} onError={err => console.log(err)}>
-        {rateAccount => (
-          <Rater
-            total={5}
-            onRate={rate => rateUser(rate, rateAccount, id)}
-            rating={average}
-            interactive={interactive}
-          />
-        )}
-      </Mutation>
+      <Rater
+        total={5}
+        onRate={({ type, rating }) => {
+          if (type === 'click') {
+            rateAccount({ variables: { id, rating } });
+          }
+        }}
+        rating={average}
+        interactive={interactive}
+      />
       <small id="stars-average">Rated {average || 0} out of 5!</small>
     </div>
   );
-};
-
-const rateUser = ({ type, rating }, rateAccount, id) => {
-  if (type === 'click') {
-    rateAccount({ variables: { id, rating } });
-  }
 };
 
 Stars.propTypes = {
