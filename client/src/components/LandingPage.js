67ce0d8c9f965b7715fc96923301f91f21e6e01d
Aaron <aaronmassey45@gmail.com>
2019-08-08 18:01:15 -0500

    Updated to use apollo hooks

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,37 +1,35 @@
 import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import { Query } from 'react-apollo';
+import { useQuery } from '@apollo/react-hooks';
 
 import CURRENT_USER from '../queries/CurrentUser';
 
-const LandingPage = () => (
-  <Query query={CURRENT_USER}>
-    {({ data, loading }) => {
-      if (loading) return <div />;
+const LandingPage = () => {
+  const { data, loading } = useQuery(CURRENT_USER);
 
-      return data.me ? (
-        <Redirect to="/home" />
-      ) : (
-        <div className="landing-page text-white row container mx-auto">
-          <div id="welcome" className="col-12 ubuntu">
-            Welcome to <div className="chowster-font display-3">Chowster</div>
-          </div>
-          <div className="col-6">
-            Not a member?
-            <div>
-              <Link to="/signup">Sign up!</Link>
-            </div>
-          </div>
-          <div className="col-6">
-            Have an account?
-            <div>
-              <Link to="/login">Log in!</Link>
-            </div>
-          </div>
+  if (loading) return <div />;
+
+  return data.me ? (
+    <Redirect to="/home" />
+  ) : (
+    <div className="landing-page text-white row container mx-auto">
+      <div id="welcome" className="col-12 ubuntu">
+        Welcome to <div className="chowster-font display-3">Chowster</div>
+      </div>
+      <div className="col-6">
+        Not a member?
+        <div>
+          <Link to="/signup">Sign up!</Link>
         </div>
-      );
-    }}
-  </Query>
-);
+      </div>
+      <div className="col-6">
+        Have an account?
+        <div>
+          <Link to="/login">Log in!</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default LandingPage;
