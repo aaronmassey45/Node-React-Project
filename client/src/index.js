b3df87220849a2ac55ed27d91168d3fc3fb17977
Aaron <aaronmassey45@gmail.com>
2019-08-09 16:01:35 -0500

    Moved graphql setup to graphql config file

--- client/src/index.js
+++ client/src/index.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient, { InMemoryCache } from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 
 import App from './App';
+import client from './graphql/config';
+
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-rater/lib/react-rater.css';
 import './App.scss';
-
-const cache = new InMemoryCache();
-
-const client = new ApolloClient({
-  cache,
-  credentials: 'same-origin',
-  uri: '/api',
-  request: context => {
-    const token = localStorage.getItem('x-auth');
-    context.setContext({
-      headers: {
-        'x-auth': token,
-      },
-    });
-  },
-});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
