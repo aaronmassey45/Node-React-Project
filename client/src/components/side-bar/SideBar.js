1c8c2b80553739f6f1acfcc8d1d433d06ea8cbf1
Aaron <aaronmassey45@gmail.com>
2019-08-13 13:27:09 -0500

    Added side bar component

---
+++ client/src/components/side-bar/SideBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import WhoToFollow from '../who-to-follow/WhoToFollow';
+
+import './side-bar.styles.scss';
+
+const SideBar = () => {
+  return (
+    <div id="side-bar">
+      <WhoToFollow />
+    </div>
+  );
+};
+
+export default SideBar;
