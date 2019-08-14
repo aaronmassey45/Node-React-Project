45d755a2b9e5eb825d6025dd386ae8f58b3025f2
Aaron <aaronmassey45@gmail.com>
2019-08-13 21:53:20 -0500

    Now takes username variable

--- client/src/graphql/queries/getFollowers.js
+++ client/src/graphql/queries/getFollowers.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 const GET_FOLLOWERS = gql`
-  query getFollowers($getUsersFollowing: Boolean = false) {
-    getFollowers(getUsersFollowing: $getUsersFollowing) {
+  query getFollowers($getUsersFollowing: Boolean = false, $username: String!) {
+    getFollowers(getUsersFollowing: $getUsersFollowing, username: $username) {
       id
       username
       bio
