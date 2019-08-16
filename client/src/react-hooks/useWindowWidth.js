35309c551beea9f2eaa011f6a48fefd989cd519b
Aaron <aaronmassey45@gmail.com>
2019-08-15 19:42:15 -0500

    Utilize debounce function to stop unecessary re-renders

--- client/src/react-hooks/useWindowWidth.js
+++ client/src/react-hooks/useWindowWidth.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 
+import debounce from '../utils/debounce';
+
 const useWindowWidth = () => {
   const [width, setWidth] = useState(window.innerWidth);
   useEffect(() => {
     const handleResize = () => setWidth(window.innerWidth);
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener('resize', debounce(handleResize, 500));
 
     return () => {
       window.removeEventListener('resize', handleResize);
