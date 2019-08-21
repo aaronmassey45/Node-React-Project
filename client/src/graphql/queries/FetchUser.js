80049fc847977091a753fce642ad879593d4a703
Aaron <aaronmassey45@gmail.com>
2019-08-20 21:49:50 -0500

    Updated query to add it _creator for likedPosts

--- client/src/graphql/queries/FetchUser.js
+++ client/src/graphql/queries/FetchUser.js
@@ -3,31 +3,44 @@ import gql from 'graphql-tag';
 export default gql`
   query fetchUser($id: ID, $username: String) {
     user(id: $id, username: $username) {
-      id
       bio
-      isAFoodTruck
-      location
-      profileImg
-      following {
-        id
-      }
       followers {
         id
       }
+      following {
+        id
+      }
+      id
+      isAFoodTruck
+      likedPosts {
+        _creator {
+          id
+          username
+          profileImg
+        }
+        id
+        likedBy {
+          id
+        }
+        text
+        timeCreated
+      }
+      location
+      posts {
+        id
+        likedBy {
+          id
+        }
+        text
+        timeCreated
+      }
+      profileImg
       rating {
         average
         numberOfRatings
         totalRating
       }
       username
-      posts {
-        id
-        text
-        timeCreated
-        likedBy {
-          id
-        }
-      }
     }
   }
 `;
