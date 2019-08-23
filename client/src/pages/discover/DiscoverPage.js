2e4356ec209aedb8ecc3b5facc0ea2c21f7c0886
Aaron <aaronmassey45@gmail.com>
2019-08-23 08:42:23 -0500

    Created bare DiscoverPage component

---
+++ client/src/pages/discover/DiscoverPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './discover.styles.scss';
+
+const DiscoverPage = props => {
+  return <div>Discover Page</div>;
+};
+
+DiscoverPage.propTypes = {};
+
+export default DiscoverPage;
