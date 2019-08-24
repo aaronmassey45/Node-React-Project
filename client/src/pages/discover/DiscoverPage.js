86c7da6c19a83f720af0bcaefc861a4eaf462990
Aaron <aaronmassey45@gmail.com>
2019-08-23 23:57:27 -0500

    Added WhoToFollow to discover page if there is no sidebar rendered

--- client/src/pages/discover/DiscoverPage.js
+++ client/src/pages/discover/DiscoverPage.js
@@ -3,18 +3,22 @@ import { useLazyQuery } from '@apollo/react-hooks';
 
 import SearchForm from './search/SearchForm';
 import SearchResults from './search/SearchResults';
+import WhoToFollow from '../../components/who-to-follow/WhoToFollow';
 import SEARCH from '../../graphql/queries/search';
+import useWindowWidth from '../../react-hooks/useWindowWidth';
 
 import './discover.styles.scss';
 
 const DiscoverPage = () => {
   const [searchQuery, { called, loading, data }] = useLazyQuery(SEARCH);
+  const windowWidth = useWindowWidth();
 
   return (
     <div id="discover-page">
       <h2>Explore Chowster</h2>
       <SearchForm searchQuery={searchQuery} />
       {called && !loading ? <SearchResults results={data.search} /> : null}
+      {windowWidth <= 992 && <WhoToFollow />}
     </div>
   );
 };
