92852208a3cd309e70d124f89d68bf4ea6aa9e19
Aaron <aaronmassey45@gmail.com>
2019-08-24 11:02:25 -0500

    Only render WhoToFollow if there is a currently authenticated user

--- client/src/pages/discover/DiscoverPage.js
+++ client/src/pages/discover/DiscoverPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { useLazyQuery } from '@apollo/react-hooks';
+import { useLazyQuery, useQuery } from '@apollo/react-hooks';
 
 import SearchForm from './search/SearchForm';
 import SearchResults from './search/SearchResults';
 import WhoToFollow from '../../components/who-to-follow/WhoToFollow';
+import CURRENT_USER from '../../graphql/queries/CurrentUser';
 import SEARCH from '../../graphql/queries/search';
 import useWindowWidth from '../../react-hooks/useWindowWidth';
 
@@ -11,14 +12,19 @@ import './discover.styles.scss';
 
 const DiscoverPage = () => {
   const [searchQuery, { called, loading, data }] = useLazyQuery(SEARCH);
+  const {
+    data: { currentUser },
+  } = useQuery(CURRENT_USER, { fetchPolicy: 'cache-only' });
   const windowWidth = useWindowWidth();
+
+  const shouldShow = currentUser && currentUser.me && windowWidth <= 992;
 
   return (
     <div id="discover-page">
       <h2>Explore Chowster</h2>
       <SearchForm searchQuery={searchQuery} />
       {called && !loading ? <SearchResults results={data.search} /> : null}
-      {windowWidth <= 992 && <WhoToFollow />}
+      {shouldShow && <WhoToFollow />}
     </div>
   );
 };
