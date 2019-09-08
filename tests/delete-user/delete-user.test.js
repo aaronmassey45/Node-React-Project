fc961ebcde5a32a48d3955f3e9c36eacff160ee4
Aaron <aaronmassey45@gmail.com>
2019-09-08 05:16:53 -0500

    Updated to use testing db.js

--- tests/delete-user/delete-user.test.js
+++ tests/delete-user/delete-user.test.js
@@ -1,71 +1,12 @@
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
 const request = require('supertest');
 
 const app = require('../../app');
 const deleteUserMutation = require('./delete-user.mutation');
 const User = require('../../models/user');
 const Post = require('../../models/post');
+const { setupDatabase, userOne } = require('../db');
 
-const userOneId = new mongoose.Types.ObjectId();
-const userOne = {
-  _id: userOneId,
-  username: 'teamosupremeo',
-  email: 'team@supreme.com',
-  isAFoodTruck: false,
-  password: 'mchammeRtIMe?!99',
-  tokens: [
-    {
-      access: 'auth',
-      token: jwt.sign(
-        { _id: userOneId, access: 'auth' },
-        process.env.JWT_SECRET
-      ),
-    },
-  ],
-};
-
-const userTwoId = new mongoose.Types.ObjectId();
-const userTwo = {
-  _id: userTwoId,
-  username: 'teamoaverage0',
-  email: 'team@average0.com',
-  isAFoodTruck: true,
-  password: 'backpackbananas',
-  tokens: [
-    {
-      access: 'auth',
-      token: jwt.sign(
-        { _id: userTwoId, access: 'auth' },
-        process.env.JWT_SECRET
-      ),
-    },
-  ],
-};
-
-const postOne = {
-  _creator: userOneId,
-  text: 'Howdy yall',
-};
-
-const postTwo = {
-  _creator: userTwoId,
-  text: 'Chowster is great, can not believe only one person worked on this!',
-};
-
-beforeEach(async done => {
-  await new User(userOne).save();
-  await new User(userTwo).save();
-  await new Post(postOne).save();
-  await new Post(postTwo).save();
-  done();
-});
-
-afterEach(async done => {
-  await User.deleteMany();
-  await Post.deleteMany();
-  done();
-});
+beforeEach(setupDatabase);
 
 test('Should delete user and their posts', async () => {
   const res = await request(app)
@@ -73,12 +14,12 @@ test('Should delete user and their posts', async () => {
     .set('x-auth', userOne.tokens[0].token)
     .send({ query: deleteUserMutation });
 
-  const user = await User.findById(userOneId);
-  const posts = await Post.find({ _creator: userOneId });
+  const user = await User.findById(userOne._id);
+  const posts = await Post.find({ _creator: userOne._id });
 
   expect(user).toBeNull();
   expect(posts).toHaveLength(0);
-  expect(res.body.data.deleteUser).toBe(userOneId.toString());
+  expect(res.body.data.deleteUser).toBe(userOne._id.toString());
 });
 
 test('Should not delete unauthenticated user', async () => {
