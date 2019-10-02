c5af251eb944c2fe0cdfd01896d1630894c0fbe1
Aaron <aaronmassey45@gmail.com>
2019-10-02 08:18:44 -0500

    Added useUnifiedTopology: true to remove error

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 require('../models/user');
