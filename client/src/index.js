eab49e22539b7b1ba7c8e6b05bad893eb38d4282
Aaron <aaronmassey45@gmail.com>
2019-12-30 12:58:20 -0600

    Update import paths

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 
-import App from './App';
-import client from './graphql/config';
+import App from 'App';
+import client from 'graphql/config';
 
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-rater/lib/react-rater.css';
-import './App.scss';
+import 'App.scss';
 
 ReactDOM.render(
   <ApolloProvider client={client}>
