45432dc9a9b80f950f0eea68156d00751e658656
Aaron <aaronmassey45@gmail.com>
2019-12-30 12:57:13 -0600

    Updated import paths

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,11 @@ import React, { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 
-import { default as Navbar } from './components/navbar/NavbarContainer';
-import Routes from './Routes';
-import SideBar from './components/side-bar/SideBar';
-import Spinner from './components/spinner/Spinner';
-
-import CURRENT_USER from './graphql/queries/CurrentUser';
+import { default as Navbar } from 'components/navbar/NavbarContainer';
+import SideBar from 'components/side-bar/SideBar';
+import Spinner from 'components/spinner/Spinner';
+import CURRENT_USER from 'graphql/queries/CurrentUser';
+import Routes from 'Routes';
 
 const App = () => {
   const { loading, data } = useQuery(CURRENT_USER, {
